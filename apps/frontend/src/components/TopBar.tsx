3aa75e7dce195b084ccbc5f01c132bcaa6e1a88c
TCAGM-Dev <thomas@andringa.org>
2025-10-02 19:27:57 +0200

    Rename Header to TopBar + improvements

---
+++ apps/frontend/src/components/TopBar.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router";
+import ThemeSelector from "./ThemeSelector";
+import Themes from "../Themes";
+import { alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+export default function TopBar({
+	links,
+}: {
+	links: { [name: string]: string };
+}) {
+	const navigate = useNavigate();
+
+	return (
+		<header
+			style={{
+				padding: "1em",
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-evenly",
+				alignItems: "center",
+				backgroundColor: alpha("#888888", 0.05),
+			}}
+		>
+			<nav>
+				{Object.entries(links).map((entry) => {
+					const [name, path] = entry;
+					return (
+						<Button
+							onClick={(e) => {
+								navigate(path);
+							}}
+						>
+							{name}
+						</Button>
+					);
+				})}
+			</nav>
+			<ThemeSelector
+				style={{
+					justifySelf: "end",
+				}}
+				themes={[Themes.LIGHT, Themes.DARK]}
+			/>
+		</header>
+	);
+}
