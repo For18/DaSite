ac30b57af3ea66774c1ba70e87830310c1c5bb4f
TCAGM-Dev <thomas@andringa.org>
2025-10-02 19:15:18 +0200

    Add range() and useTime()

--- apps/frontend/src/util.ts
+++ apps/frontend/src/util.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const RANDOM_CHARACTER_SET =
 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
@@ -15,4 +17,28 @@ export function generateRandomString(length: number): string {
 	}
 
 	return result;
+}
+
+export function range(amount: number): number[] {
+	const result: number[] = Array(amount);
+	for (let i = 0; i < amount; i++) {
+		result.push(i);
+	}
+	return result;
+}
+
+export function useTime() {
+	const [time, setTime] = useState(Date.now());
+	useEffect(() => {
+		let animationFrameId: number;
+		function update() {
+			setTime(Date.now());
+			animationFrameId = requestAnimationFrame(update);
+		}
+		animationFrameId = requestAnimationFrame(update);
+		return () => {
+			cancelAnimationFrame(animationFrameId);
+		};
+	});
+	return time;
 }
