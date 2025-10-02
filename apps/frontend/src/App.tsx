0ab57375fef20f6de1ee9e60649f9ba95da6ef7b
TCAGM-Dev <thomas@andringa.org>
2025-10-02 19:19:41 +0200

    Add clock and fallback routes Implement lazy loading Improve theme management

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,22 +1,56 @@
-import Home from "./routes/Home";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Layout from "./Layout";
+import Layout from "./components/Layout";
 import { Theme, ThemeProvider } from "@mui/material/styles";
-import Themes from "./themes";
-import { useState } from "react";
+import Themes, { getThemeById } from "./Themes";
+import { createContext, lazy, Suspense, useState } from "react";
+import Throbber from "./components/Throbber";
+import NotFound from "./routes/NotFound";
+const Home = lazy(() => import("./routes/Home"));
+const Clock = lazy(() => import("./routes/Clock"));
+
+export const SetThemeContext = createContext<(theme: Theme) => void>(() => {});
 
 export default function App() {
-	const [theme, setTheme] = useState<Theme>(Themes.LIGHT);
+	const [theme, setThemeState] = useState<Theme>(
+		getThemeById(window.localStorage.getItem("theme")) ?? Themes.LIGHT
+	);
+
+	function setTheme(theme: Theme) {
+		setThemeState(theme);
+		window.localStorage.setItem("theme", theme.id);
+	}
 
 	return (
 		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Layout>
-					<Routes>
-						<Route index element={<Home />} />
-					</Routes>
-				</Layout>
-			</BrowserRouter>
+			<SetThemeContext.Provider value={setTheme}>
+				<BrowserRouter>
+					<Layout>
+						<Routes>
+							<Route
+								index
+								element={
+									<Suspense fallback={<Throbber />}>
+										<Home />
+									</Suspense>
+								}
+							/>
+							<Route
+								path="/clock"
+								element={
+									<Suspense fallback={<Throbber />}>
+										<Clock />
+									</Suspense>
+								}
+							/>
+
+							<Route
+								path="*"
+								element={<NotFound />}
+							/>
+						</Routes>
+					</Layout>
+				</BrowserRouter>
+			</SetThemeContext.Provider>
 		</ThemeProvider>
 	);
 }
