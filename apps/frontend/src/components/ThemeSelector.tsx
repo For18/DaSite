c728d225d7302eb525a3caadb37677c84306371a
TCAGM-Dev <thomas@andringa.org>
2025-10-02 19:27:40 +0200

    Add ThemeSelector

---
+++ apps/frontend/src/components/ThemeSelector.tsx
@@ -0,0 +1,41 @@
+import { Theme, useTheme } from "@mui/material/styles";
+import { useContext } from "react";
+import { SetThemeContext } from "../App";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import { getThemeById } from "../Themes";
+
+export default function ThemeSelector({
+	themes,
+	style,
+}: {
+	themes: Theme[];
+	style?: React.CSSProperties;
+}) {
+	const setTheme = useContext(SetThemeContext);
+	const currentTheme = useTheme();
+
+	return (
+		<FormControl style={style}>
+			<InputLabel id="theme-selector-label">Theme</InputLabel>
+			<Select
+				labelId="theme-selector-label"
+				id="theme-selector"
+				value={currentTheme.id}
+				label="Theme"
+				style={{
+					width: "7em",
+				}}
+				onChange={(event) => {
+					setTheme(getThemeById(event.target.value) ?? themes[0]);
+				}}
+			>
+				{themes.map((theme) => (
+					<MenuItem value={theme.id}>{theme.name}</MenuItem>
+				))}
+			</Select>
+		</FormControl>
+	);
+}
