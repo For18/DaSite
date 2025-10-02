461e688b80762d97309fde62fba5f029e2b68eb6
TCAGM-Dev <thomas@andringa.org>
2025-10-02 19:17:51 +0200

    Move and improve Layout

---
+++ apps/frontend/src/components/Layout.tsx
@@ -0,0 +1,34 @@
+import TopBar from "./TopBar";
+import Footer from "./Footer";
+import { PropsWithChildren } from "react";
+import { useTheme } from "@mui/material/styles";
+
+export default function Layout({ children }: PropsWithChildren) {
+	const theme = useTheme();
+
+	return (
+		<div
+			style={{
+				backgroundColor: theme.palette.background.default,
+				minHeight: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "space-between",
+			}}
+		>
+			<TopBar
+				links={{
+					Home: "/",
+				}}
+			/>
+			<article
+				style={{
+					alignSelf: "center",
+				}}
+			>
+				{children}
+			</article>
+			<Footer />
+		</div>
+	);
+}
