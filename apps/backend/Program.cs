c030b4f12c5f545a3893ca911d231a7e787bdb8d
TCAGM-Dev <thomas@andringa.org>
2025-10-03 16:27:57 +0200

    Fix dependencies

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -1,5 +1,9 @@
-using Microsoft.OpenAPI.Models;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string apiName = "API";
+string apiVersionString = "v1";
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,7 +12,7 @@ builder.Services.AddRouting();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+	c.SwaggerDoc(apiVersionString, new OpenApiInfo { Title = apiName, Version = apiVersionString });
 });
 
 var app = builder.Build();
@@ -19,12 +23,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
 	{
-		c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", apiName + " " + apiVersionString);
 	});
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
