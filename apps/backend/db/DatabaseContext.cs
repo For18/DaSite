7b033a31c600d76d0ec7dbc2feee253381bbdbd0
TCAGM-Dev <thomas@andringa.org>
2025-10-04 18:36:00 +0200

    Remove example logic in backend

--- apps/backend/db/DatabaseContext.cs
+++ apps/backend/db/DatabaseContext.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 public class DatabaseContext : DbContext
 {
-	public DbSet<Student> Students { get; set; }
-
 	protected override void OnConfiguring(DbContextOptionsBuilder contextBuilder)
 	{
 		contextBuilder.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING") ?? "");
--- apps/backend/db/Student.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class Student
-{
-	[Key]
-	public long Studentnummer { get; set; }
-	[StringLength(30)]
-	public required string Naam { get; set; }
-}
