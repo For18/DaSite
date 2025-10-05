820f3a1e98a4161ef66fb9817b75306c33c86c03
TCAGM-Dev <thomas@andringa.org>
2025-10-05 14:56:27 +0200

    Add backend:AccountController.cs

---
+++ apps/backend/controllers/AccountController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("/account")]
+public class AccountController : ControllerBase
+{
+	[HttpGet("login")]
+	public async Task Login(string returnUrl = "/")
+	{
+		var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+			.WithRedirectUri("./")
+		.Build();
+
+		await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+	}
+
+	[HttpGet("logout")]
+	[Authorize]
+	public async Task Logout()
+	{
+		var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+			.WithRedirectUri("/")
+		.Build();
+
+		await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, HttpContext.User);
+		await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+	}
+}
