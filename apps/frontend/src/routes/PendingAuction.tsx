5b3b536a8a3b073dda3f040b84074a2d63a2da79
Matt-hhs <24071765@student.hhs.nl>
2025-10-27 11:48:07 +0100

    Add base files and pendingauction page

--- apps/frontend/src/routes/PendingAuction.tsx
+++ apps/frontend/src/routes/PendingAuction.tsx
@@ -1,31 +1,53 @@
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { Auction, useAPI } from "../lib/api";
 import { randomCharacter } from "../lib/util";
 
 export default function PendingAuction() {
-	const [text, setText] = useState("Pending Auctions");
-
-	useEffect(() => {
-		document.title = "For18 - Pending Auction";
-	});
+	const auctions = useAPI<Auction[]>("/auctions/pending");
+	const items = [];
+	for(let i = 0; i < 6; i++){
+		items.push(
+			<div grid-area={"item"+1}>
+				<Typography>
+					Pending Auction {i+1}
+				</Typography>
+			</div>
+		);
+	}
 
 	return (
-		<>
-			<Header level={1} color="textPrimary">
-				{text}
-			</Header>
-			<Typography color="textPrimary">"If that's beef, then I'm from Bangladesh"</Typography>
-			<Button
-				variant="contained"
-				onClick={() => {
-					setText(text + randomCharacter());
-				}}
-				disabled={text.length >= 50}
-			>
-				Update title
-			</Button>
-		</>
+		<Paper sx={{
+			width: "800px",
+			maxWidth: "80%",
+			minHeight: "300px",
+			display: "grid",
+			gridTemplateColumns: "auto auto",
+			gap: "16px",
+			padding: "16px"
+
+		}}>
+			
+			{items}
+			<div grid-area="menu" color="red">
+				<Typography color="textPrimary">
+					Pending Auctions (under construction)
+				</Typography>
+			</div>
+			<div grid-area="content" color="blue">
+				<Typography color="textPrimary">
+					Ik wil kaas
+				</Typography>
+			</div>
+			<div grid-area="shit" color="green">
+				<Typography color="textPrimary">
+					En nog meer kaas
+				</Typography>
+			</div>
+		</Paper>
+		
 	);
 }
