9c5a6cf9b4e01ba181b259907954dd6f3fcbbaf3
Julius Schutte <Julius.Schutte@gmail.com>
2025-10-28 15:41:56 +0100

    Update Clock route

--- apps/frontend/src/routes/Clock.tsx
+++ apps/frontend/src/routes/Clock.tsx
@@ -12,36 +12,35 @@ export default function Clock() {
 	// if (auction == undefined) return <NotFound/>;
 
 	// const product = useAPI<Product>("/product" + auction.productId);
-	const auction: Auction = {
-		id: 1,
-		count: 1,
-		batchSize: 1,
-		startingPrice: 100000,
-		minimumPrice: 1000,
-		startingTime: 1,
-		productId: 1,
-		plannerId: 1
-	};
+  const auction: Auction = {
+    id: 1,
+    count: 1,
+    batchSize: 1,
+    startingPrice: 100000,
+    minimumPrice: 1000,
+    startingTime: 1,
+    productId: 1,
+    plannerId: 1,
+  } 
 	const product: Product = {
 		id: 1,
 		name: "this is a product name",
-		description: "this is a cool description",
-		thumbnailImageUrl:
-			"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgdbenterprises.com%2Fwp-content%2Fuploads%2F2011%2F12%2Fgreen-beans-1.jpg&f=1&nofb=1&ipt=56b8996de43de72482cda0064592b39d61cb78a5b480014b3e6b2a1f98203341",
-		imageUrls: ["https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F58%2F91%2F9b%2F58919b9d620bcbf61500bb25deeea962.jpg&f=1&nofb=1&ipt=34f39ee8283d317de6ccbfb28ce8b91b854b0b8287cc879ecb1da2cb636266bc", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F5d%2Fc2%2F4e%2F5dc24ed911e04089c6a19ebc595036c7.jpg&f=1&nofb=1&ipt=7e82689cc0f4b4fa5ab8fc6ccab87cc67772f6d1ed9bdb989768a0813ab56f21"],
+		description: "description",
+		thumbnailImageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgdbenterprises.com%2Fwp-content%2Fuploads%2F2011%2F12%2Fgreen-beans-1.jpg&f=1&nofb=1&ipt=56b8996de43de72482cda0064592b39d61cb78a5b480014b3e6b2a1f98203341",
+		imageUrls: ["url1", "url2"],
 		owner: 123
 	};
 
 	return (
-		<div
+    <div
 			style={{
 				width: "100%",
-				height: "100%",
+        height: "100%",
 				display: "grid",
 				gridTemplateColumns: "1fr 1fr",
 				gridGap: "20px",
 				marginBottom: "100px",
-				alignItems: "start"
+				alignItems: "start",
 			}}
 		>
 			{/* Left Column Clock */}
@@ -51,7 +50,7 @@ export default function Clock() {
 					justifyContent: "center",
 					alignItems: "center",
 					paddingLeft: "20px",
-					paddingTop: "200px"
+          paddingTop: "200px"
 				}}
 			>
 				<AuctionClock Auction={auction}/>
@@ -65,7 +64,7 @@ export default function Clock() {
 					alignItems: "flex-start",
 					padding: "20px",
 					maxHeight: "calc(100vh - 100px)",
-					overflowY: "auto"
+					overflowY: "auto",
 				}}
 			>
 				{product == null ? <Throbber/> : <ProductView product={product}/>}
