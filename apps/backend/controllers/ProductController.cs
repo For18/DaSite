81fc16a42f6ac13cb26832915929fdd7bd83edce
Julius Schutte <Julius.Schutte@gmail.com>
2025-10-29 15:55:33 +0100

    Split ProductController.cs

--- apps/backend/controllers/ProductController.cs
+++ apps/backend/controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.JsonPatch;
 using System.ComponentModel;
@@ -12,8 +13,10 @@ public class ProductExternal {
 		ThumbnailImageId = thumbnailImageId;
 		OwnerId = ownerId;
 	}
-	public ProductExternal(Product product)
-		: this(product.Id, product.Name, product.Description, product.ThumbnailImage?.Id, product.Owner.Id) { }
+
+	public static ProductExternal ToExternal(Product product) {
+    return new ProductExternal(product.Id, product.Name, product.Description, product.ThumbnailImage?.Id, product.Owner.Id);
+  }
 
 	public Product ToProduct(DatabaseContext db) {
 		return new Product {
@@ -37,18 +40,18 @@ public class ProductController : ControllerBase {
 	[HttpGet("{id}")]
 	public ActionResult<ProductExternal> Get(ulong id) {
 		using (var db = new DatabaseContext()) {
-			Product? product = db.Products.Where(product => product.Id == id).FirstOrDefault();
+			Product? product = db.Products.Include(product => product.Owner).Where(product => product.Id == id).FirstOrDefault();
 
 			if (product == null) return NotFound();
 
-			return new ProductExternal(product);
+			return ProductExternal.ToExternal(product);
 		}
 	}
 
 	[HttpGet("/products")]
 	public ActionResult<ProductExternal[]> GetAll() {
 		using (var db = new DatabaseContext()) {
-			return db.Products.Select(product => new ProductExternal(product)).ToArray();
+			return db.Products.Include(product => product.Owner).Select(product => ProductExternal.ToExternal(product)).ToArray();
 		}
 	}
 
@@ -97,81 +100,3 @@ public class ProductController : ControllerBase {
 	}
 }
 
-[DisplayName(nameof(ProductImage))]
-public class ProductImageExternal {
-	public ProductImageExternal(ProductImage prodImage) {
-		Id = prodImage.Id;
-		Parent = prodImage.Parent.Id;
-		Url = prodImage.Url;
-	}
-	public ProductImage ToProductImage(DatabaseContext db) {
-		return new ProductImage {
-			Id = Id,
-			Parent = db.Products.Where(parent => parent.Id == Id).First(),
-			Url = Url
-		};
-	}
-	public ulong Id { get; init; }
-	public ulong Parent { get; init; }
-	public string Url { get; init; }
-}
-
-
-[ApiController]
-[Route("product-image")]
-public class ProductImageController : ControllerBase {
-	[HttpGet("{id}")]
-	public ActionResult<ProductImageExternal> Get(ulong id) {
-		using (var db = new DatabaseContext()) {
-			ProductImage? prodImage = db.ProductImages.Where(image => image.Id == id).FirstOrDefault();
-
-			if (prodImage == null) return NotFound();
-
-			return new ProductImageExternal(prodImage);
-		}
-	}
-
-	[HttpPost]
-	public ActionResult Post(ProductImageExternal productImageData) {
-		using (var db = new DatabaseContext()) {
-			if (db.ProductImages.Any(image => image.Id == productImageData.Id)) return Conflict("Already exists");
-
-			ProductImage prodImage = productImageData.ToProductImage(db);
-
-			db.ProductImages.Add(prodImage);
-			db.SaveChanges();
-
-			return Ok(new IdReference(prodImage.Id));
-		}
-	}
-
-	[HttpDelete("{id}")]
-	public ActionResult Delete(ulong id) {
-		using (var db = new DatabaseContext()) {
-			ProductImage? prodImage = db.ProductImages.Find(id);
-			if (prodImage == null) return NotFound();
-
-			db.ProductImages.Remove(prodImage);
-			db.SaveChanges();
-
-			return NoContent();
-		}
-	}
-
-	[HttpPatch("{id}")]
-	public ActionResult Patch(ulong id, [FromBody] JsonPatchDocument<ProductImage> patchdoc) {
-		using (var db = new DatabaseContext()) {
-			ProductImage? prodImage = db.ProductImages.Find(id);
-			if (prodImage == null) return NotFound();
-
-			patchdoc.ApplyTo(prodImage, ModelState);
-
-			if (!ModelState.IsValid) {
-				return BadRequest(ModelState);
-			}
-
-			db.SaveChanges();
-			return Ok(prodImage);
-		}
-	}
-}
