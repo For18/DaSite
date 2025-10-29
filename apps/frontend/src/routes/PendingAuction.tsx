6d66214db1396a66b593a670c5a55f0e46dbc3b4
Matt-hhs <24071765@student.hhs.nl>
2025-10-29 18:07:01 +0100

    Add title Pending Products

--- apps/frontend/src/routes/PendingAuction.tsx
+++ apps/frontend/src/routes/PendingAuction.tsx
@@ -11,41 +11,54 @@ export default function PendingAuction() {
 	const auctions = [
 		{id: 1, count: 10, batchSize: 5, startingPrice: 100, minimumPrice: 20, startingTime: 50, productId: 1, plannerId: 1}, 
 		{id: 2, count: 9, batchSize: 5, startingPrice: 500, minimumPrice: 20, startingTime: 50, productId: 2, plannerId: 2},
+		{id: 3, count: 8, batchSize: 5, startingPrice: 300, minimumPrice: 20, startingTime: 50, productId: 3, plannerId: 3}, 
+		{id: 2, count: 9, batchSize: 5, startingPrice: 500, minimumPrice: 20, startingTime: 50, productId: 2, plannerId: 2},
+		{id: 3, count: 8, batchSize: 5, startingPrice: 300, minimumPrice: 20, startingTime: 50, productId: 3, plannerId: 3}, 
+		{id: 2, count: 9, batchSize: 5, startingPrice: 500, minimumPrice: 20, startingTime: 50, productId: 2, plannerId: 2},
+		{id: 3, count: 8, batchSize: 5, startingPrice: 300, minimumPrice: 20, startingTime: 50, productId: 3, plannerId: 3}, 
+		{id: 2, count: 9, batchSize: 5, startingPrice: 500, minimumPrice: 20, startingTime: 50, productId: 2, plannerId: 2},
 		{id: 3, count: 8, batchSize: 5, startingPrice: 300, minimumPrice: 20, startingTime: 50, productId: 3, plannerId: 3}];
 
 	const minPaperHeight = 300;
 
 	return (
-		<Paper sx={{
-			width: "800px",
-			maxWidth: "80vw",
-			minHeight: `${minPaperHeight}px`,
-			display: auctions != null && auctions.length > 0 ? "grid" : "flex",
-			gridTemplateColumns: screenWidth > 1000 ? "1fr 1fr" : "1fr",
-			justifyContent: "center",
-			alignItems: "flex-start",
-			gap: "16px",
-			padding: "16px"
-		}}>
-			{auctions == null ?
-				<Throbber/> :
-				auctions.length == 0 ?
-				<div style={{
-					alignItems: "center",
-					display: "flex",
-					justifyContent: "center",
-					minHeight: `${minPaperHeight}px`
-				}}>
-				<Typography color="textPrimary">No pending auctions</Typography>
-				</div> :
-				auctions.map(auction => <PendingAuctionCard auction={auction}/>)}
-			{/*
-			{test == null ?
-				<Throbber/> :
-				test.length == 0 ?
-				<Typography color="textPrimary">No pending auctions</Typography> :
-				test?.map(testing => <PendingAuctionCard testing={testing}/>)}
-			*/}
-		</Paper>
+		<div style={{
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+			}}>
+			<h1>Pending Products</h1>
+			<Paper sx={{
+				width: "800px",
+				maxWidth: "80vw",
+				minHeight: `${minPaperHeight}px`,
+				display: auctions != null && auctions.length > 0 ? "grid" : "flex",
+				gridTemplateColumns: screenWidth > 1000 ? "1fr 1fr" : "1fr",
+				justifyContent: "center",
+				alignItems: "flex-start",
+				gap: "16px",
+				padding: "16px"
+			}}>
+				{auctions == null ?
+					<Throbber/> :
+					auctions.length == 0 ?
+					<div style={{
+						alignItems: "center",
+						display: "flex",
+						justifyContent: "center",
+						minHeight: `${minPaperHeight}px`
+					}}>
+					<Typography color="textPrimary">No pending auctions</Typography>
+					</div> :
+					auctions.map(auction => <PendingAuctionCard auction={auction}/>)}
+				{/*
+				{test == null ?
+					<Throbber/> :
+					test.length == 0 ?
+					<Typography color="textPrimary">No pending auctions</Typography> :
+					test?.map(testing => <PendingAuctionCard testing={testing}/>)}
+				*/}
+			</Paper>
+		</div>
 	);
 }
