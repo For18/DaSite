ab318bcee9443be486ece72deba2c1bc44e584ae
Matt-hhs <24071765@student.hhs.nl>
2025-10-29 15:01:03 +0100

    Add divs with styles

--- apps/frontend/src/components/PendingAuctionCard.tsx
+++ apps/frontend/src/components/PendingAuctionCard.tsx
@@ -1,3 +1,4 @@
+import Typography from "@mui/material/Typography";
 import { Auction } from "../lib/api";
 
 export default function PendingAuctionCard({ auction }: {
@@ -10,11 +11,31 @@ export default function PendingAuctionCard({ auction }: {
 			aspectRatio: "4/1"
 		}}>
 			<div style={{
-				alignSelf: "Left"
+				float: "left",
+				width: "21%",
+				height: "9vw",
+				backgroundColor: "blue",
+				paddingLeft: "2%",
+				paddingRight: "2%",
+				alignContent: "center",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center"
 			}}>
-				<p>Seller: </p>
-				<p>Asking price: {auction.startingPrice}</p>
-				<p>Amount: {auction.batchSize * auction.count}</p>
+				<img src="https://i.ibb.co/7xnxXSC6/matt.webp" width={80} height={80}></img>
+				{/* Insert product image here */}
+			</div>
+			<div style={{
+				float: "right", 
+				width: "70%",
+				height: "9vw",
+				paddingLeft: "5%",
+				
+
+			}}>
+				<Typography color="textPrimary">Seller: </Typography>
+				<Typography color="textPrimary">Asking price: {auction.startingPrice}</Typography>
+				<Typography color="textPrimary">Amount: {auction.batchSize * auction.count}</Typography>
 			</div>
 		</div>
 	);
