4be1758473a2500b89be4fe1dd8b1429d5cdf96f
Julius Schutte <Julius.Schutte@gmail.com>
2025-10-31 23:14:01 +0100

    Add dedicated *.css

--- apps/frontend/src/components/ProductView.tsx
+++ apps/frontend/src/components/ProductView.tsx
@@ -1,64 +1,54 @@
 import { Product } from "../lib/api";
-import { API_URL, Auction, useAPI, User } from "../lib/api";
+import { useAPI, User, ProductImage } from "../lib/api";
+import Throbber from "../components/Throbber";
+import NotFound from "../routes/NotFound";
+import "./styles/ProductView.css"
 
 export default function ProductView({ product }: { product: Product }) {
 	const owner = useAPI<User>("/user/" + product.owner) ?? null;
+  const prodImages = useAPI<ProductImage[]>("/product-image/from/" + product.id);
+  product.imageUrls = [];
+
+  if (owner === null) return <Throbber/>;
+  if (owner === undefined) return <NotFound/>;
+
+  if (prodImages === null) return <Throbber/>;
+  if (prodImages === undefined) return <NotFound/>;
+  
+  prodImages.forEach((prodImage) => product.imageUrls.push(prodImage.url));
 
 	return (
-		<div
-			style={{
-				padding: "20px",
-				fontFamily: "Arial, sans-serif",
-				lineHeight: "1.6"
-			}}
-		>
+		<div className={"product-view"}>
 			<div>
 				<h2>{product.name}</h2>
-				<p style={{ fontSize: "1.1em", color: "#555" }}>
+				<p className={"seller"}>
 					Seller: {owner ? owner.displayName : "Seller not found"}
 				</p>
 			</div>
 
-			<hr style={{ border: "1px solid #ccc", margin: "20px 0" }}/>
+			<hr className={"horizontal-rule"}/>
 
 			<div>
 				<p>{product.description}</p>
 			</div>
 
-			<hr style={{ border: "1px solid #ccc", margin: "20px 0" }}/>
+			<hr className={"horizontal-rule"}/>
 
 			<div>
 				<img
+          className={"thumbnail-image"}
 					src={product.thumbnailImageUrl}
 					alt={product.name}
-					style={{
-						width: "100%",
-						height: "auto",
-						borderRadius: "8px"
-					}}
 				/>
 			</div>
 
-			<div
-				style={{
-					display: "grid",
-					gridTemplateColumns: "repeat(auto-fill, minmax(100px, 1fr))",
-					gap: "10px",
-					marginTop: "20px"
-				}}
-			>
+			<div className={"extra-image-container"}>
 				{product.imageUrls.map((url, index) => (
 					<div key={index} style={{ cursor: "pointer" }}>
 						<img
+              className={"extra-image"}
 							src={url}
 							alt={`Product Image ${index + 1}`}
-							style={{
-								width: "100%",
-								height: "auto",
-								borderRadius: "8px",
-								boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
-								transition: "transform 0.3s ease"
-							}}
 							onClick={() => window.open(url, "_blank")}
 						/>
 					</div>
--- apps/frontend/src/routes/ClockPage.tsx
+++ apps/frontend/src/routes/ClockPage.tsx
@@ -6,6 +6,7 @@ import Throbber from "../components/Throbber";
 import { Auction, Product, useAPI } from "../lib/api";
 import { useTime } from "../lib/util";
 import NotFound from "./NotFound";
+import "./styles/ClockPage.css"
 
 export default function ClockPage() {
   const { auctionId } = useParams();
@@ -46,42 +47,14 @@ export default function ClockPage() {
   const formattedMilliseconds = remainingMilliSec < 100 ? `0${remainingMilliSec}`.padStart(3, '0') : `${remainingMilliSec}`;
 
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr",
-        gridGap: "20px",
-        marginBottom: "100px",
-        alignItems: "start"
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          paddingLeft: "20px",
-          paddingTop: "200px"
-        }}
-      >
+    <div className={"base-container"}>
+      <div className={"clock-container"}>
         <Clock price={currentPrice} seconds={formattedSeconds} milliseconds={formattedMilliseconds} progress={auctionProgress} />
       </div>
 
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          padding: "20px",
-          maxHeight: "calc(100vh - 100px)",
-          overflowY: "auto"
-        }}
-      >
+      <div className={"product-container"}>
         {product == null ? <Throbber /> : <ProductView product={product} />}
       </div>
     </div>
   );
 }
-
