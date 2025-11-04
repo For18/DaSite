a6fc945a48ccb0738ce81eef4b8370f5179d5acc
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-04 13:36:01 +0100

    Add clock overlay

--- apps/frontend/src/components/Clock.tsx
+++ apps/frontend/src/components/Clock.tsx
@@ -5,13 +5,34 @@ import "./styles/Clock.css";
 export default function Clock(
 	{ price, duration, progress }: { price: string, duration: string, progress: number }
 ) {
-	const classNameAppend = progress < 25 ? "three-quarter" : (progress < 50 ? "halfway" : "");
+  const currencyType = "100 cent";
 
 	return (
 		<div className={"container"}>
-			<div className={`clock ${classNameAppend}`}>
-				{price}
+			<div className={"clock"} style={{ "--progress": progress } as React.CSSProperties}>
+        <div className={"clock-overlay"}>
+
+          {/* Top Box*/}
+          <div className={"clock-box currency"}> 
+            <Typography> currency </Typography>
+            <Typography> {currencyType} </Typography>
+          </div>
+  
+          {/* Middle Box*/}
+          <div className={"clock-box price"}> 
+            <Typography> price </Typography>
+            <Typography> {price} </Typography>
+          </div>
+  
+          {/* Bottom Box*/}
+          <div className={"clock-box count"}> 
+            <Typography> count </Typography>
+            <Typography> {6969} </Typography>
+          </div>
+
+        </div>
 			</div>
+
 			<Typography className={"typography"}>
         {duration}
 			</Typography>
