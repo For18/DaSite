1c2dbaa984719cbfe1ca51892b0a939e0d017f42
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-04 13:35:15 +0100

    Update time argument format

--- apps/frontend/src/components/Clock.tsx
+++ apps/frontend/src/components/Clock.tsx
@@ -1,9 +1,9 @@
+import React from "react";
 import { Typography } from "@mui/material";
 import "./styles/Clock.css";
 
 export default function Clock(
-	{ price, seconds, milliseconds, progress }: { price: string, seconds: string, milliseconds: string,
-		progress: number }
+	{ price, duration, progress }: { price: string, duration: string, progress: number }
 ) {
 	const classNameAppend = progress < 25 ? "three-quarter" : (progress < 50 ? "halfway" : "");
 
@@ -13,7 +13,7 @@ export default function Clock(
 				{price}
 			</div>
 			<Typography className={"typography"}>
-				{seconds}:{milliseconds}
+        {duration}
 			</Typography>
 		</div>
 	);
