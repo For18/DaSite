0c8dcc0c27c73e57a8b50d03055f384df51f86eb
Matt-hhs <24071765@student.hhs.nl>
2025-11-04 13:21:57 +0100

    Fix AuctionController

--- apps/backend/controllers/AuctionController.cs
+++ apps/backend/controllers/AuctionController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.JsonPatch;
 using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 
 [DisplayName(nameof(Auction))]
 public class AuctionExternal {
