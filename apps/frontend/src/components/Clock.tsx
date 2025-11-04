29680a223385b6ebe951bf5b84c484764bb08049
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-04 13:38:52 +0100

    dprint fmt

--- apps/frontend/src/components/Clock.tsx
+++ apps/frontend/src/components/Clock.tsx
@@ -1,40 +1,38 @@
-import React from "react";
 import { Typography } from "@mui/material";
+import React from "react";
 import "./styles/Clock.css";
 
 export default function Clock(
 	{ price, duration, progress }: { price: string, duration: string, progress: number }
 ) {
-  const currencyType = "100 cent";
+	const currencyType = "100 cent";
 
 	return (
 		<div className={"container"}>
 			<div className={"clock"} style={{ "--progress": progress } as React.CSSProperties}>
-        <div className={"clock-overlay"}>
+				<div className={"clock-overlay"}>
+					{/* Top Box*/}
+					<div className={"clock-box currency"}>
+						<Typography>currency</Typography>
+						<Typography>{currencyType}</Typography>
+					</div>
 
-          {/* Top Box*/}
-          <div className={"clock-box currency"}> 
-            <Typography> currency </Typography>
-            <Typography> {currencyType} </Typography>
-          </div>
-  
-          {/* Middle Box*/}
-          <div className={"clock-box price"}> 
-            <Typography> price </Typography>
-            <Typography> {price} </Typography>
-          </div>
-  
-          {/* Bottom Box*/}
-          <div className={"clock-box count"}> 
-            <Typography> count </Typography>
-            <Typography> {6969} </Typography>
-          </div>
+					{/* Middle Box*/}
+					<div className={"clock-box price"}>
+						<Typography>price</Typography>
+						<Typography>{price}</Typography>
+					</div>
 
-        </div>
+					{/* Bottom Box*/}
+					<div className={"clock-box count"}>
+						<Typography>count</Typography>
+						<Typography>{6969}</Typography>
+					</div>
+				</div>
 			</div>
 
 			<Typography className={"typography"}>
-        {duration}
+				{duration}
 			</Typography>
 		</div>
 	);
--- apps/frontend/src/routes/ClockPage.tsx
+++ apps/frontend/src/routes/ClockPage.tsx
@@ -1,24 +1,26 @@
-import { useEffect, useMemo} from "react";
+import { useEffect, useMemo } from "react";
 import { useParams } from "react-router";
 import Clock from "../components/Clock";
 import ProductView from "../components/ProductView";
 import Throbber from "../components/Throbber";
-import { Auction, Product, useAPI, API_URL } from "../lib/api";
+import { API_URL, Auction, Product, useAPI } from "../lib/api";
 import { useTime } from "../lib/util";
 import NotFound from "./NotFound";
 import "./styles/ClockPage.css";
 
 function lerp(from: number, to: number, t: number): number {
-  return from + t * (to - from);
+	return from + t * (to - from);
 }
 
 function formatDuration(duration: number): string {
-    if (duration < 0) return '00:000';
-    
-    const milliseconds = Math.round(duration % 1000);
-    const seconds = Math.floor(duration / 1000) % 60;
+	if (duration < 0) return "00:000";
 
-    return `${(seconds < 10 ? "0" : "") + seconds}:${(milliseconds < 100 ? "0" : "") + (milliseconds < 10 ? "0" : "") + milliseconds}`;
+	const milliseconds = Math.round(duration % 1000);
+	const seconds = Math.floor(duration / 1000) % 60;
+
+	return `${(seconds < 10 ? "0" : "") + seconds}:${
+		(milliseconds < 100 ? "0" : "") + (milliseconds < 10 ? "0" : "") + milliseconds
+	}`;
 }
 
 export default function ClockPage() {
@@ -26,34 +28,37 @@ export default function ClockPage() {
 	const auction = useAPI<Auction>("/auction/" + auctionId);
 	const product = useAPI<Product>(auction ? "/product/" + auction.productId : null);
 
-  useEffect(() => {
-      fetch(API_URL + "/auction/" + auction?.id, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify([{op: "replace", path: "/startingTime", value: Math.round(Date.now() + 5000)}])
-      }).then(() => console.log("patched"))
-  }, [auction]);
-  
+	useEffect(() => {
+		fetch(API_URL + "/auction/" + auction?.id, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify([{ op: "replace", path: "/startingTime", value: Math.round(Date.now() + 5000) }])
+		}).then(() => console.log("patched"));
+	}, [auction]);
+
 	const startingTime = useMemo(() => {
-      return auction?.startingTime; 
-  }, [auction]);
+		return auction?.startingTime;
+	}, [auction]);
 
 	const currentTime = useTime();
-  const auctionLenMillis = auction ? auction?.length * 1000 : 1;
+	const auctionLenMillis = auction ? auction?.length * 1000 : 1;
 
-  const elapsedTime = (auction && startingTime != null) ? currentTime - startingTime : 0;
+	const elapsedTime = (auction && startingTime != null) ? currentTime - startingTime : 0;
 
-  const auctionProgress = (auction == null) ? 0 : elapsedTime / auctionLenMillis;
+	const auctionProgress = (auction == null) ? 0 : elapsedTime / auctionLenMillis;
 
 	if (auction === undefined) return <NotFound/>;
 	if (auction === null) return <Throbber/>;
 	if (product === null) return <Throbber/>;
 
-  const currentPrice = Math.min(Math.max(lerp(auction.startingPrice, auction.minimumPrice , auctionProgress), auction.minimumPrice), auction.startingPrice).toFixed(2);
-  const remainingTime = auctionLenMillis - elapsedTime;
-  const formattedTime = remainingTime > auctionLenMillis ? formatDuration(0) : formatDuration(remainingTime);
+	const currentPrice = Math.min(
+		Math.max(lerp(auction.startingPrice, auction.minimumPrice, auctionProgress), auction.minimumPrice),
+		auction.startingPrice
+	).toFixed(2);
+	const remainingTime = auctionLenMillis - elapsedTime;
+	const formattedTime = remainingTime > auctionLenMillis ? formatDuration(0) : formatDuration(remainingTime);
 
 	return (
 		<div className={"base-container"}>
@@ -61,7 +66,7 @@ export default function ClockPage() {
 				<Clock price={currentPrice} duration={formattedTime} progress={auctionProgress}/>
 			</div>
 
-      <div className={"container-separator"}/>
+			<div className={"container-separator"}/>
 
 			<div className={"product-container"}>
 				{product == null ? <Throbber/> : <ProductView product={product}/>}
