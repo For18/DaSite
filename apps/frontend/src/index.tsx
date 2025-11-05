72bff1d42a7abd77ae63927ed96a4b8e97e78159
TCAGM-Dev <thomas@andringa.org>
2025-11-05 10:54:00 +0100

    Enable strict mode in frontend

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 
@@ -5,4 +6,8 @@ const rootNode = document.getElementById("root");
 
 if (rootNode == null) throw new Error("No root node found");
 
-ReactDom.createRoot(rootNode).render(<App/>);
+ReactDom.createRoot(rootNode).render(
+	<StrictMode>
+		<App/>
+	</StrictMode>
+);
