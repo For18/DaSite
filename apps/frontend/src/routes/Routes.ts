c7d16226e636ef97cef86edd041bdb20ad2cf5c1
TCAGM-Dev <thomas@andringa.org>
2025-11-06 10:30:14 +0100

    Add /profile route

--- apps/frontend/src/routes/Routes.ts
+++ apps/frontend/src/routes/Routes.ts
@@ -3,9 +3,11 @@ import React, { JSX, lazy } from "react";
 const Home = lazy(() => import("./Home"));
 const Clock = lazy(() => import("./Clock"));
 const Auctions = lazy(() => import("./Auctions"));
+const Profile = lazy(() => import("./Profile"));
 
 export default {
 	"/": Home,
 	"/clock/:auctionId": Clock,
-	"/auctions": Auctions
+	"/auctions": Auctions,
+	"/profile/:userId": Profile
 } as { [path: string]: React.LazyExoticComponent<() => JSX.Element> };
