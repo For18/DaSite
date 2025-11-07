c48093cd9d39ef32c71a721cd437acfadad3848a
TCAGM-Dev <thomas@andringa.org>
2025-11-07 16:12:45 +0100

    Improve Button component styling

--- apps/frontend/src/components/Button.tsx
+++ apps/frontend/src/components/Button.tsx
@@ -1,14 +1,19 @@
 import { JSX, MouseEventHandler, PropsWithChildren } from "react";
+import styles from "./Button.module.scss";
 
 export interface ButtonProps extends PropsWithChildren {
-	variant?: "contained" | "outlined" | "text";
+	variant?: "text" | "outlined" | "contained";
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 	disabled?: boolean;
 	color?: "brand" | "success" | "warning" | "error";
 }
 
-export default function Button({ onClick, children: content, disabled = false }: ButtonProps): JSX.Element {
-	return <button onClick={e => {
+export default function Button({ onClick, children: content, disabled = false, variant = "text", color = "brand" }: ButtonProps): JSX.Element {
+	return <button className={[
+		styles.button,
+		styles[variant],
+		styles[color]
+	].join(" ")} onClick={e => {
 		if (disabled) return;
 		onClick?.(e);
 	}} disabled={disabled}>{content}</button>;
