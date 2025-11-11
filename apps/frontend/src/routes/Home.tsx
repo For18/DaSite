e7f607942a679ae50aa7d36b9c1d972e7f2177cf
Matt-hhs <24071765@student.hhs.nl>
2025-11-11 11:28:51 +0100

    Add general layout

--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -2,9 +2,13 @@ import { useCallback, useEffect, useState } from "react";
 import Button from "../components/Button";
 import Typography from "../components/Typography";
 import { randomCharacter } from "../lib/util";
+import { useScreenSize } from "../lib/util";
+import styles from "./Home.module.scss";
 
 export default function Home() {
 	const [text, setText] = useState("Hello world");
+
+	const [screenWidth, screenHeight] = useScreenSize();
 
 	useEffect(() => {
 		document.title = "For18 - Home";
@@ -16,38 +20,40 @@ export default function Home() {
 
 	return (
 		<>
-			<Typography heading={1}>
-				{text}
-			</Typography>
-			<Typography>
-				"If Hypixel has taught me something, it's that if you have a problem, the answer is slavery" -
-				Technoblade (idk when) (bonjour)
-			</Typography>
-			<table>
-				{["contained", "outlined", "text"].map(variant => (
-					<tr key={variant}>
-						{["brand", "success", "warning", "error"].map(color => (
-							<td key={color}>
-								<Button
-									variant={variant as any}
-									color={color as any}
-									onClick={updateText}
-									disabled={text.length >= 50}
-								>
-									Button
-								</Button>
-							</td>
-						))}
-					</tr>
-				))}
-			</table>
-			<Button
-				variant="contained"
-				color="error"
-				onClick={() => setText("Hello world")}
-			>
-				Reset
-			</Button>
+			<div className={styles.main}>
+				<div className={styles.left}>
+					<div className={styles.header}>
+						<Typography heading={1} color="primary" >
+							Flora Holland Veiling
+						</Typography>
+					</div>
+					
+					<div className={styles.subHeader}>
+						<Typography heading={2} color="secondary">
+							Welkom bij de (online) veiling site van Flora Holland
+						</Typography>
+					</div>
+
+					<div className={styles.subTextDiv}>
+						<Typography color="primary">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</Typography>
+					</div>
+				</div>
+				<div className={styles.right}>
+					<div className={styles.header4}>
+						<Typography heading={4} color="secondary">
+							(Header 4) Info
+						</Typography>
+					</div>
+
+					<div className={styles.header4Text}>
+						<Typography color="primary">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</Typography>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
