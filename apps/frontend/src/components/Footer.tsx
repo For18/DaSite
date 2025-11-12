c02ccd8bb2f3eb957cd321817410c4196f8144ef
Matt-hhs <24071765@student.hhs.nl>
2025-11-12 16:41:15 +0100

    Add better styling to footer

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -4,25 +4,31 @@ import Typography from "./Typography";
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<div className={styles.left}>
-				<Typography href="/auctions">
-					Contactgegevens
-				</Typography>
-				<Typography href="/profile/1">
-					Gebruikers
-				</Typography>
-				<Typography href="">
-					Openingstijden
-				</Typography>
-			</div>
-			<Typography color="secondary">
-				{"© "}
-				{new Date().getFullYear()} For18
-			</Typography>
-			<div className={styles.right}>
-				<Typography href="">
-					Locaties
-				</Typography>
+			<div className={styles.container}>
+				<div className={styles.top}>
+					<Typography color="secondary">
+						{"© "}
+						{new Date().getFullYear()} For18
+					</Typography>
+				</div>
+				<div className={styles.bottom}>
+					<div className={styles.left}>
+						<Typography href="/auctions">
+							Contactgegevens
+						</Typography>
+						<Typography href="/profile/1">
+							Gebruikers
+						</Typography>
+						<Typography href="">
+							Openingstijden
+						</Typography>
+					</div>
+					<div className={styles.right}>
+						<Typography href="">
+							Locaties
+						</Typography>
+					</div>
+				</div>
 			</div>
 		</footer>
 	);
