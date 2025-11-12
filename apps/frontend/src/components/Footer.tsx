e93430c2a94f63ed596b18762526ba1f7ce92de7
Matt-hhs <24071765@student.hhs.nl>
2025-11-12 10:37:13 +0100

    Add footer text + better styles

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -4,10 +4,26 @@ import Typography from "./Typography";
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
+			<div className={styles.left}>
+				<Typography>
+					Contactgegevens
+				</Typography>
+				<Typography>
+					Gebruikers
+				</Typography>
+				<Typography>
+					Openingstijden
+				</Typography>
+			</div>
 			<Typography color="secondary">
 				{"© "}
 				{new Date().getFullYear()} For18
 			</Typography>
+			<div className={styles.right}>
+				<Typography>
+					Locaties
+				</Typography>
+			</div>
 		</footer>
 	);
 }
