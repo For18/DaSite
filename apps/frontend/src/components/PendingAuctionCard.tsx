49ba3cc685c6f25bc5ac182053246fd7e34f9ad6
Matt-hhs <24071765@student.hhs.nl>
2025-11-13 12:09:24 +0100

    Add fallback image

--- apps/frontend/src/components/PendingAuctionCard.tsx
+++ apps/frontend/src/components/PendingAuctionCard.tsx
@@ -7,11 +7,12 @@ export default function PendingAuctionCard({ auction }: { auction: Auction }) {
 	const product = useAPI<Product>("/product/" + auction.productId);
 	const user = useAPI<User>("/user/" + auction.plannerId);
 	const thumbnailImage = useAPI<ProductImage>(product ? "/product-image/" + product.id : null);
+	const thumbnailUrl = thumbnailImage?.url ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCBHgbS23kyBw2r8Pquu19UtKZnrZmFUx1g&s";
 
 	return (
 		<div className={styles.card}>
 			<div className={styles.image}>
-				<img className={styles.productImage} src={thumbnailImage?.url} />
+				<img className={styles.productImage} src={thumbnailUrl} />
 			</div>
 			<div className={styles.infoCard}>
 				<div className={styles.productNameContainer}>
