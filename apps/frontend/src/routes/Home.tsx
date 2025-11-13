20231e977965deb06d8dc1efe210591712253dd5
Matt-hhs <24071765@student.hhs.nl>
2025-11-13 13:28:43 +0100

    Fix description text

--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -31,9 +31,14 @@ export default function Home() {
 						<Typography heading={2} color="secondary">
 							Welkom bij de veiling site van Royal FloraHolland
 						</Typography>
-						<Typography>
-							Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem! Dit is de website van Royal FloraHolland. We verkopen bloemen door middel van ons online veilingsysteem!
-						</Typography>
+						<div>
+							<Typography>
+								Bloemen en planten maken onze wereld groener. Ze kleuren onze huizen, tuinen en buurten en vergroenen onze steden. Een groene omgeving maakt mensen gelukkig en het werkt stressverlagend. <br/><br/>
+								Onze coöperatie is een twee-eenheid: we zijn een vereniging én een bedrijf. <br/><br/>
+								We hebben twee doelen: een gezonde en aantrekkelijke coöperatie voor onze leden zijn en blijven én bouwen aan het grootste internationale B2B platform binnen de sierteeltsector. <br/><br/>
+								Duurzaamheid is hierbij een belangrijk fundament. Door bundeling van kennis en kracht bereiken we duurzaam succes, internationale groei en versterken we de coöperatie. Om voorop te blijven lopen, nu en in de toekomst.
+							</Typography>
+						</div>
 					</div>
 				</div>
 				<div className={styles.right}>
