407f94378be349ea155e3e32aeaf10e6d3a5cccb
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-19 10:52:40 +0100

    Update Auction tables for multiple Products

---
+++ apps/backend/db/DatabaseContext.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseContext : DbContext {
+	public DbSet<Product> Products { get; set; }
+	public DbSet<ProductImage> ProductImages { get; set; }
+	public DbSet<User> Users { get; set; }
+	public DbSet<Auction> Auctions { get; set; }
+  public DbSet<AuctionEntry> AuctionEntry { get; set; }
+  public DbSet<AuctionItem> AuctionItem { get; set; }
+	public DbSet<Sale> Sales { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder contextBuilder) {
+		contextBuilder.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING") ?? "");
+	}
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.Entity<AuctionEntry>()
+          .HasKey(ae => new { ae.AuctionId, ae.AuctionItemId });
+  
+      modelBuilder.Entity<AuctionEntry>()
+          .HasOne(ae => ae.Auction)
+          .WithMany()
+          .HasForeignKey(ae => ae.AuctionId)
+          .OnDelete(DeleteBehavior.Cascade);
+  
+      modelBuilder.Entity<AuctionEntry>()
+          .HasOne(ae => ae.AuctionItem)
+          .WithMany()
+          .HasForeignKey(ae => ae.AuctionItemId)
+          .OnDelete(DeleteBehavior.Cascade);
+  }
+}
---
+++ apps/backend/db/tables/Auction.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+public class AuctionItem {
+	[Key]
+	[Required]
+	public required ulong Id { get; set; }
+
+	[Required]
+	public required ushort Count { get; set; }
+
+	[Required]
+	public required uint BatchSize { get; set; }
+
+	[Required]
+	public required uint StartingPrice { get; set; }
+
+	[Required]
+	public required uint MinimumPrice { get; set; }
+
+  [Required]
+  public required Product Product { get; set; }
+
+  [Required]
+  public required uint Length { get; set; }
+}
+
+public class AuctionEntry {
+  [Key]
+  [Required]
+  [ForeignKey("AuctionId")]
+  [DeleteBehavior(DeleteBehavior.NoAction)]
+  public required Auction Auction { get; set; }
+
+  [Key]
+  [Required]
+  [ForeignKey("AuctionItemId")]
+  [DeleteBehavior(DeleteBehavior.NoAction)]
+  public required AuctionItem AuctionItem { get; set; }
+
+} 
+
+public class Auction {
+  [Key]
+  [Required]
+  public required ulong Id { get; set; }
+
+  [ForeignKey("PlannerId")]
+  [DeleteBehavior(DeleteBehavior.NoAction)]
+  public virtual User? Planner { get; set; }
+
+  public ulong? StartingTime { get; set; }
+
+}
