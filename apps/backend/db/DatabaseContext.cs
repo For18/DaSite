c85257c189cb31612199fcc64d9cf1391c61a6aa
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-19 10:52:40 +0100

    Update Auction tables for multiple Products

--- apps/backend/db/DatabaseContext.cs
+++ apps/backend/db/DatabaseContext.cs
@@ -6,9 +6,28 @@ public class DatabaseContext : DbContext {
 	public DbSet<ProductImage> ProductImages { get; set; }
 	public DbSet<User> Users { get; set; }
 	public DbSet<Auction> Auctions { get; set; }
+  public DbSet<AuctionEntry> AuctionEntry { get; set; }
+  public DbSet<AuctionItem> AuctionItem { get; set; }
 	public DbSet<Sale> Sales { get; set; }
 
 	protected override void OnConfiguring(DbContextOptionsBuilder contextBuilder) {
 		contextBuilder.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING") ?? "");
 	}
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.Entity<AuctionEntry>()
+          .HasKey(ae => new { ae.AuctionId, ae.AuctionItemId });
+  
+      modelBuilder.Entity<AuctionEntry>()
+          .HasOne(ae => ae.Auction)
+          .WithMany()
+          .HasForeignKey(ae => ae.AuctionId)
+          .OnDelete(DeleteBehavior.Cascade);
+  
+      modelBuilder.Entity<AuctionEntry>()
+          .HasOne(ae => ae.AuctionItem)
+          .WithMany()
+          .HasForeignKey(ae => ae.AuctionItemId)
+          .OnDelete(DeleteBehavior.Cascade);
+  }
 }
