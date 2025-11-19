7c7a10d96732187ec92fada30046b274b998eb3a
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-19 10:52:40 +0100

    Update Auction tables for multiple Products

--- apps/backend/db/DatabaseContext.cs
+++ apps/backend/db/DatabaseContext.cs
@@ -8,34 +8,28 @@ public class DatabaseContext : DbContext {
 	public DbSet<ProductImage> ProductImages { get; set; }
 	public DbSet<User> Users { get; set; }
 	public DbSet<Auction> Auctions { get; set; }
-	public DbSet<AuctionEntry> AuctionEntries { get; set; }
-	public DbSet<AuctionItem> AuctionItems { get; set; }
+  public DbSet<AuctionEntry> AuctionEntry { get; set; }
+  public DbSet<AuctionItem> AuctionItem { get; set; }
 	public DbSet<Sale> Sales { get; set; }
 
 	protected override void OnConfiguring(DbContextOptionsBuilder contextBuilder) {
 		contextBuilder.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING") ?? "");
 	}
 
-	protected override void OnModelCreating(ModelBuilder modelBuilder) {
-		modelBuilder.Entity<AuctionEntry>()
-		  .HasOne(ae => ae.Auction)
-		  .WithMany()
-		  .HasForeignKey("AuctionId")
-		  .OnDelete(DeleteBehavior.Cascade);
-
-		modelBuilder.Entity<AuctionEntry>()
-		  .HasOne(ae => ae.AuctionItem)
-		  .WithMany()
-		  .HasForeignKey("AuctionItemId")
-		  .OnDelete(DeleteBehavior.Cascade);
-
-		modelBuilder.Entity<AuctionItem>()
-		  .HasOne(ai => ai.Product)
-		  .WithMany()
-		  .HasForeignKey("ProductId")
-		  .OnDelete(DeleteBehavior.Cascade);
-
-		modelBuilder.Entity<AuctionEntry>()
-		  .HasKey("AuctionId", "AuctionItemId");
-	}
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.Entity<AuctionEntry>()
+          .HasKey(ae => new { ae.AuctionId, ae.AuctionItemId });
+  
+      modelBuilder.Entity<AuctionEntry>()
+          .HasOne(ae => ae.Auction)
+          .WithMany()
+          .HasForeignKey(ae => ae.AuctionId)
+          .OnDelete(DeleteBehavior.Cascade);
+  
+      modelBuilder.Entity<AuctionEntry>()
+          .HasOne(ae => ae.AuctionItem)
+          .WithMany()
+          .HasForeignKey(ae => ae.AuctionItemId)
+          .OnDelete(DeleteBehavior.Cascade);
+  }
 }
--- apps/backend/db/tables/Auction.cs
+++ apps/backend/db/tables/Auction.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,33 +20,37 @@ public class AuctionItem {
 	[Required]
 	public required uint MinimumPrice { get; set; }
 
-	[Required]
-	[ForeignKey("ProductId")]
-	public required Product Product { get; set; }
+  [Required]
+  public required Product Product { get; set; }
 
-	[Required]
-	public required uint Length { get; set; }
+  [Required]
+  public required uint Length { get; set; }
 }
 
-// TODO: add restrict element thingy so planned in products cannot get deleted
 public class AuctionEntry {
-	[Required]
-	[ForeignKey("AuctionId")]
-	public required Auction Auction { get; set; }
+  [Key]
+  [Required]
+  [ForeignKey("AuctionId")]
+  [DeleteBehavior(DeleteBehavior.NoAction)]
+  public required Auction Auction { get; set; }
 
-	[Required]
-	[ForeignKey("AuctionItemId")]
-	public required AuctionItem AuctionItem { get; set; }
-}
+  [Key]
+  [Required]
+  [ForeignKey("AuctionItemId")]
+  [DeleteBehavior(DeleteBehavior.NoAction)]
+  public required AuctionItem AuctionItem { get; set; }
+
+} 
 
 public class Auction {
-	[Key]
-	[Required]
-	public required ulong Id { get; set; }
+  [Key]
+  [Required]
+  public required ulong Id { get; set; }
 
-	[ForeignKey("PlannerId")]
-	public virtual User? Planner { get; set; }
+  [ForeignKey("PlannerId")]
+  [DeleteBehavior(DeleteBehavior.NoAction)]
+  public virtual User? Planner { get; set; }
 
-	public ulong? StartingTime { get; set; }
+  public ulong? StartingTime { get; set; }
 
 }
