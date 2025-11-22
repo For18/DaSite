a1731697d2cf0e1ede4f866d0614a2fac4dbce02
TCAGM-Dev <thomas@andringa.org>
2025-11-22 11:25:17 +0100

    Move identity endpoints to `/identity`

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -38,7 +38,7 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapIdentityApi<User>();
+app.MapGroup("identity").MapIdentityApi<User>();
 app.MapControllers();
 
 if (app.Environment.IsDevelopment()) {
