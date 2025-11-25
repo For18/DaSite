ec5207dff24f55b44a0930566b264a7cb2c11f26
Julius Schutte <Julius.Schutte@gmail.com>
2025-11-25 10:36:25 +0100

    Update API routes

--- apps/frontend/src/components/PendingAuctionCard.tsx
+++ apps/frontend/src/components/PendingAuctionCard.tsx
@@ -2,15 +2,20 @@ import { type Auction, type Product, type ProductImage, useAPI, type User } from
 import Image from "./Image";
 import styles from "./PendingAuctionCard.module.scss";
 import Typography from "./Typography";
+import Throbber from "./Throbber";
 
 export default function PendingAuctionCard({ auction }: { auction: Auction }) {
-	const product = useAPI<Product>("/product/" + auction.productId);
+	const item = useAPI<AuctionItem>("/auction-item/get-by-auction/" + auction.id);
 	const user = useAPI<User>("/user/" + auction.plannerId);
+  const product = useAPI<Product>(item ? "/product/" + item.productId : null);
 	const thumbnailImage = useAPI<ProductImage[]>(product ? "/product-image/from/" + product.id : null);
 	const thumbnailUrl = thumbnailImage && thumbnailImage[0].url ?
 		thumbnailImage[0].url :
 		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCBHgbS23kyBw2r8Pquu19UtKZnrZmFUx1g&s";
 
+  if (item === null) return <Throbber/>;
+  if (item === undefined) return null;
+  
 	return (
 		<div className={styles.card}>
 			<div className={styles.image}>
@@ -25,10 +30,10 @@ export default function PendingAuctionCard({ auction }: { auction: Auction }) {
 						Seller: {user?.userName}
 					</Typography>
 					<Typography color="secondary" className={styles.infoText}>
-						Asking price: €{auction.startingPrice / 100},-
+						Asking price: €{item.startingPrice / 100},-
 					</Typography>
 					<Typography color="secondary" className={styles.infoText}>
-						Amount: {auction.batchSize * auction.count}
+						Amount: {item.batchSize * item.count}
 					</Typography>
 				</div>
 			</div>
