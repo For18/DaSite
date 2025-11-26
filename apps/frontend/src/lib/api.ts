e6ae879321b40f465c6907932c88cf28a37539f9
TCAGM-Dev <thomas@andringa.org>
2025-11-26 11:09:52 +0100

    Fix useAPI route

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -58,7 +58,7 @@ export interface User {
 }
 
 export function useAPI<T>(route: string | null): T | null | undefined {
-	const {isLoading, value, error} = usePromise<T>(() => route !== null ? fetch(route).then(response => response.json()) : null, [route]);
+	const {isLoading, value, error} = usePromise<T>(() => route !== null ? fetch(API_URL + route).then(response => response.json()) : null, [route]);
 
 	if (isLoading) return null;
 	if (error) return undefined;
