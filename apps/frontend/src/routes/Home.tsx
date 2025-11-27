fb842a703e2c6b683494437c03a4d42c5b7a5189
Matt-hhs <24071765@student.hhs.nl>
2025-11-27 10:52:47 +0100

    Fix incorrect merge changes

--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -16,42 +16,27 @@ export default function Home() {
 			</div>
 
 			<div className={styles.main}>
-				<div className={styles.left}>
-					<div className={styles.subHeader}>
-						<Typography heading={2} color="secondary">
-							Welkom bij de veiling site van Royal FloraHolland
-						</Typography>
-						<div>
-							<Typography>
-								Bloemen en planten maken onze wereld groener. Ze kleuren onze huizen, tuinen en buurten
-								en vergroenen onze steden. Een groene omgeving maakt mensen gelukkig en het werkt
-								stressverlagend. <br/>
-								<br/>
-								Onze coöperatie is een twee-eenheid: we zijn een vereniging én een bedrijf. <br/>
-								<br/>
-								We hebben twee doelen: een gezonde en aantrekkelijke coöperatie voor onze leden zijn en
-								blijven én bouwen aan het grootste internationale B2B platform binnen de
-								sierteeltsector. <br/>
-								<br/>
-								Duurzaamheid is hierbij een belangrijk fundament. Door bundeling van kennis en kracht
-								bereiken we duurzaam succes, internationale groei en versterken we de coöperatie. Om
-								voorop te blijven lopen, nu en in de toekomst.
+				<div className={styles.imageBackground}>
+					<div className={styles.left}>
+						<div className={styles.subHeading}>
+							<Typography heading={2}>
+								Welkom bij de veiling site van Royal FloraHolland
 							</Typography>
 							<div>
 								<Typography>
-									Bloemen en planten maken onze wereld groener. Ze kleuren onze huizen, tuinen en
-									buurten en vergroenen onze steden. Een groene omgeving maakt mensen gelukkig en het
-									werkt stressverlagend. <br/>
+									Bloemen en planten maken onze wereld groener. Ze kleuren onze huizen, tuinen en buurten
+									en vergroenen onze steden. Een groene omgeving maakt mensen gelukkig en het werkt
+									stressverlagend. <br/>
 									<br/>
 									Onze coöperatie is een twee-eenheid: we zijn een vereniging én een bedrijf. <br/>
 									<br/>
-									We hebben twee doelen: een gezonde en aantrekkelijke coöperatie voor onze leden zijn
-									en blijven én bouwen aan het grootste internationale B2B platform binnen de
+									We hebben twee doelen: een gezonde en aantrekkelijke coöperatie voor onze leden zijn en
+									blijven én bouwen aan het grootste internationale B2B platform binnen de
 									sierteeltsector. <br/>
 									<br/>
-									Duurzaamheid is hierbij een belangrijk fundament. Door bundeling van kennis en
-									kracht bereiken we duurzaam succes, internationale groei en versterken we de
-									coöperatie. Om voorop te blijven lopen, nu en in de toekomst.
+									Duurzaamheid is hierbij een belangrijk fundament. Door bundeling van kennis en kracht
+									bereiken we duurzaam succes, internationale groei en versterken we de coöperatie. Om
+									voorop te blijven lopen, nu en in de toekomst.
 								</Typography>
 							</div>
 						</div>
