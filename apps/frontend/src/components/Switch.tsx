4e0c03eb5af4e87e3cc2f8915efaf30f290848ae
TCAGM-Dev <thomas@andringa.org>
2025-11-28 17:12:16 +0100

    Add switch

--- apps/frontend/src/components/Switch.tsx
+++ apps/frontend/src/components/Switch.tsx
@@ -1,31 +1,21 @@
-import { useCallback } from "react";
 import styles from "./Switch.module.scss";
 
 export interface ToggleSwitchProps {
 	enabled: boolean;
 	onClick: () => void;
-	labelledby?: string;
 }
 
-export function Switch({ enabled, onClick, labelledby }: ToggleSwitchProps) {
-	const onKeyDown = useCallback((e: KeyboardEvent) => {
-		if (e.key === "Enter" || e.key === " ") onClick();
-	}, [onClick]);
-
+export function Switch({ enabled, onClick }: ToggleSwitchProps) {
 	return (
 		<div
 			className={[
 				styles.container,
 				enabled ? styles.enabled : null
 			].filter(v => v != null).join(" ")}
-			aria-checked={enabled}
-			aria-labelledby={labelledby}
-			role="switch"
-			tabIndex={0}
+			aria-pressed={enabled}
 			onClick={onClick}
-			onKeyDown={onKeyDown as any}
 		>
-			<div className={styles.dot}/>
+			<div className={styles.dot}></div>
 		</div>
 	);
 }
--- apps/frontend/src/routes/Test.tsx
+++ apps/frontend/src/routes/Test.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { Switch } from "../components/Switch";
+
 export default function Test() {
-	return <>
-		
-	</>
+	const [switchEnabled, setSwitchEnabled] = useState<boolean>(false);
+
+	return (
+		<>
+			<Switch enabled={switchEnabled} onClick={() => setSwitchEnabled(e => !e)}/>
+		</>
+	);
 }
