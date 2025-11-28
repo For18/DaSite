02e24d50989b17408b4e315c625532b06b23ac77
TCAGM-Dev <thomas@andringa.org>
2025-11-28 18:40:46 +0100

    Add Checkbox

---
+++ apps/frontend/src/components/Checkbox.tsx
@@ -0,0 +1,16 @@
+import styles from "./Checkbox.module.scss";
+
+export interface CheckboxProps {
+	checked: boolean;
+	onClick: () => void;
+}
+
+export default function Checkbox({ checked, onClick }: CheckboxProps) {
+	return (
+		<div onClick={onClick} aria-pressed={checked} className={styles.container + (checked ? " " + styles.checked : "")}>
+			<svg className={styles.svg} viewBox="-20 -20 130 130">
+				<path d="M100 0 L50 90 L0 50"/>
+			</svg>
+		</div>
+	)
+}
