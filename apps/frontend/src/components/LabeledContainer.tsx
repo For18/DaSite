dbfd5ab2cc75b8924ab136dd751b1fa77b5189b9
TCAGM-Dev <thomas@andringa.org>
2025-11-28 17:54:14 +0100

    Add LabeledContainer

---
+++ apps/frontend/src/components/LabeledContainer.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import Typography from "./Typography";
+import styles from "./LabeledContainer.module.scss";
+
+export interface LabeledContainerProps {
+	color?: "primary" | "secondary";
+	text: string;
+	children: ReactNode
+}
+
+export default function LabeledContainer({ color = "secondary", text, children }: LabeledContainerProps) {
+	return (
+		<div className={styles.container}>
+			{children}
+			<Typography color={color}>{text}</Typography>
+		</div>
+	)
+}
