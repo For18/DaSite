ae6993962917900ab5b2445f62cc063b5f03347a
TCAGM-Dev <thomas@andringa.org>
2025-11-29 11:06:18 +0100

    Fix Checkbox > svg accessibility

--- apps/frontend/src/components/Checkbox.tsx
+++ apps/frontend/src/components/Checkbox.tsx
@@ -14,7 +14,7 @@ export default function Checkbox({ checked, onClick, labelledby }: CheckboxProps
 
 	return (
 		<div onClick={onClick} onKeyDown={onKeyDown as any} aria-pressed={checked} role="checkbox" tabIndex={0} aria-labelledby={labelledby} className={styles.container + (checked ? " " + styles.checked : "")}>
-			<svg className={styles.svg} viewBox="-20 -20 130 130">
+			<svg className={styles.svg} viewBox="-20 -20 130 130" role="none">
 				<path d="M100 0 L50 90 L0 50"/>
 			</svg>
 		</div>
