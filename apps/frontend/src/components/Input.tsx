1b2b6b56aef14da2dd11e033f085b392b7ec041a
TCAGM-Dev <thomas@andringa.org>
2025-11-29 10:13:27 +0100

    Add Input component

--- apps/frontend/src/components/Input.tsx
+++ apps/frontend/src/components/Input.tsx
@@ -1,27 +1,17 @@
 import { Ref } from "react";
 import styles from "./Input.module.scss";
 
-export type InputType = "text" | "textfield" | "password" | "date" | "datetime-local" | "email" | "number" | "password"
-	| "tel" | "text" | "time" | "url";
+export type InputType = "text" | "password" | "date" | "datetime-local" | "email" | "number" | "password" | "tel" | "text" | "time" | "url"
 
 export interface InputProps {
 	type: InputType;
+	ref?: Ref<HTMLInputElement>;
 	placeholder?: string;
 	disabled?: boolean;
-	labelledby?: string;
-	value?: string;
-	onChange?: (value: string) => void;
-	className?: string;
 }
 
-export default function Input(
-	{ type, placeholder, disabled, labelledby, value, onChange, className }: InputProps
-) {
-	const Element = type === "textfield" ? "textarea" : "input";
-	if (type === "textfield") type = "text";
+export default function Input({ type, ref, placeholder, disabled }: InputProps) {
 	return (
-		<Element type={type} placeholder={placeholder}
-			className={styles.input + (className != null ? " " + className : "")} disabled={disabled}
-			aria-labelledby={labelledby} value={value} onChange={e => onChange?.(e.target.value)}/>
+		<input type={type} ref={ref} placeholder={placeholder} className={styles.input} disabled={disabled}/>
 	);
 }
