ef8a1563cd302fca98477003517d08f5ff276917
TCAGM-Dev <thomas@andringa.org>
2025-11-29 10:23:27 +0100

    Add LabeledContainer.id

--- apps/frontend/src/components/LabeledContainer.tsx
+++ apps/frontend/src/components/LabeledContainer.tsx
@@ -5,14 +5,15 @@ import styles from "./LabeledContainer.module.scss";
 export interface LabeledContainerProps {
 	color?: "primary" | "secondary";
 	text: string;
-	children: ReactNode
+	children: ReactNode;
+	id?: string;
 }
 
-export default function LabeledContainer({ color = "primary", text, children }: LabeledContainerProps) {
+export default function LabeledContainer({ color = "primary", text, children, id }: LabeledContainerProps) {
 	return (
 		<div className={styles.container}>
 			{children}
-			<Typography color={color}>{text}</Typography>
+			<Typography color={color} id={id}>{text}</Typography>
 		</div>
 	)
 }
