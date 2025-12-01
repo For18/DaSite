68263ffd259d5cac01f1744eddbeb6894309c538
Lopunny <ljaleeuwen@gmail.com>
2025-12-01 12:02:59 +0100

    pending list should work with multiple auctions

--- apps/frontend/src/components/PendingAuctionCard.tsx
+++ apps/frontend/src/components/PendingAuctionCard.tsx
@@ -6,6 +6,7 @@ import Throbber from "./Throbber";
 import Typography from "./Typography";
 
 export default function PendingAuctionCard({ auction }: { auction: Auction }) {
+	const auctions = useAPI<Auction[]>("/auctions/pending");
 	const item = useAPI<AuctionItem>("/auction-item/get-by-auction/" + auction.id);
 	const user = useAPI<User>("/user/" + auction.plannerId);
 	const product = useAPI<Product>(item ? "/product/" + item.productId : null);
@@ -14,30 +15,37 @@ export default function PendingAuctionCard({ auction }: { auction: Auction }) {
 		thumbnailImage[0].url :
 		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCBHgbS23kyBw2r8Pquu19UtKZnrZmFUx1g&s";
 
-	if (item === null) return <Throbber/>;
-	if (item === undefined) return <NotFound/>;
+	if (item === null || auctions === null) return <Throbber/>;
+	if (item === undefined || auctions === undefined) return <NotFound/>;
 
 	return (
-		<div className={styles.card}>
-			<div className={styles.image}>
-				<Image className={styles.productImage} src={thumbnailUrl} alt={`Thumbnail van ${product?.name}`}/>
-			</div>
-			<div className={styles.infoCard}>
-				<div className={styles.productNameContainer}>
-					<Typography className={styles.productName}>{product?.name}</Typography>
-				</div>
-				<div className={styles.infoTextContainer}>
-					<Typography color="secondary" className={styles.infoText}>
-						Seller: {user?.displayName}
-					</Typography>
-					<Typography color="secondary" className={styles.infoText}>
-						Asking price: €{item.startingPrice ? item.startingPrice / 100 : "NaN"},-
-					</Typography>
-					<Typography color="secondary" className={styles.infoText}>
-						Amount: {item.startingPrice ? item.batchSize * item.count : "NaN"}
-					</Typography>
-				</div>
-			</div>
+		<div className={styles.pendingList}>
+			<Typography>
+				{auctions.map(auction => (
+					<div className={styles.card}>
+						<div className={styles.image}>
+							<Image className={styles.productImage} src={thumbnailUrl}
+								alt={`Thumbnail van ${product?.name}`}/>
+						</div>
+						<div className={styles.infoCard}>
+							<div className={styles.productNameContainer}>
+								<Typography className={styles.productName}>{product?.name}</Typography>
+							</div>
+							<div className={styles.infoTextContainer}>
+								<Typography color="secondary" className={styles.infoText}>
+									Seller: {user?.displayName}
+								</Typography>
+								<Typography color="secondary" className={styles.infoText}>
+									Asking price: €{item.startingPrice ? item.startingPrice / 100 : "NaN"},-
+								</Typography>
+								<Typography color="secondary" className={styles.infoText}>
+									Amount: {item.startingPrice ? item.batchSize * item.count : "NaN"}
+								</Typography>
+							</div>
+						</div>
+					</div>
+				))}
+			</Typography>
 		</div>
 	);
 }
