da79d4c7f2b92e38dc64f5c287f49e3095d63503
TCAGM-Dev <thomas@andringa.org>
2025-12-01 17:30:55 +0100

    Add accessability to Slider

--- apps/frontend/src/components/Slider.tsx
+++ apps/frontend/src/components/Slider.tsx
@@ -2,11 +2,13 @@ import { ReactElement, useEffect, useRef, useState } from "react";
 import styles from "./Slider.module.scss";
 import { useMousePosition } from "../lib/util";
 
-export interface SliderProps {
+export interface SliderProps { // TODO: Add direction prop (aria-direction, keybinds & CSS adjustments)
 	min: number;
 	max: number;
 	value: number;
 	onChange?: (value: number) => void;
+	valueText?: string;
+	labelledBy: string;
 	step?: number;
 	marks?: boolean | number[] | {[value: number]: ReactElement}
 	width?: string;
@@ -14,27 +16,31 @@ export interface SliderProps {
 	//tooltip?: boolean | ((value: number) => string); // implement when tooltip component is added
 }
 
-export default function Slider({ min, max, value, onChange, step, marks = false, width, disabled = false }: SliderProps) {
+export default function Slider({ min, max, value, onChange, valueText, labelledBy, step, marks = false, width, disabled = false }: SliderProps) {
 	const [dragging, setDragging] = useState<boolean>(false);
 	const [mouseX, mouseY] = useMousePosition();
 	const rectRef = useRef<HTMLDivElement>(null);
 
 	const range = max - min;
 
-	function updateValue() {
-		if (disabled) return;
+	function setValue(value: number) {
 		if (onChange == null) return;
-		if (!dragging) return;
-		if (rectRef.current == null) return;
-		const barRect = rectRef.current.getBoundingClientRect();
-		const progress = (mouseX - barRect.x) / barRect.width;
-		let value = progress * range + min;
 		if (value > max) value = max;
 		else if (value < min) value = min;
 		if (step != null) value = Math.round((value - min) / step) * step + min;
 		onChange(value);
 	}
-	useEffect(updateValue, [mouseX]);
+
+	function updateValueFromMouse() {
+		if (disabled) return;
+		if (!dragging) return;
+		if (rectRef.current == null) return;
+		const barRect = rectRef.current.getBoundingClientRect();
+		const progress = (mouseX - barRect.x) / barRect.width;
+		let value = progress * range + min;
+		setValue(value);
+	}
+	useEffect(updateValueFromMouse, [mouseX]);
 
 	function drag() {
 		window.addEventListener("mouseup", undrag);
@@ -52,10 +58,10 @@ export default function Slider({ min, max, value, onChange, step, marks = false,
 	const progress = (value - min) / range;
 
 	return (
-		<div className={styles.container} style={{width}}>
+		<div className={styles.container} style={{width}} role="slider" aria-valuemin={min} aria-valuemax={max} aria-valuenow={value} aria-disabled={disabled} aria-valuetext={valueText} aria-labelledby={labelledBy}>
 			<div className={styles.bar} onMouseDown={() => {
 				drag();
-				updateValue();
+				updateValueFromMouse();
 			}}>
 				<div className={styles.fill} style={{"--fill": progress} as any}/>
 			</div>
@@ -63,6 +69,37 @@ export default function Slider({ min, max, value, onChange, step, marks = false,
 				e.stopPropagation();
 				e.preventDefault();
 				if (e.button === 0) drag();
+			}} tabIndex={0} onKeyDown={e => {
+				if (onChange == null) return;
+				if (disabled) return;
+				switch (e.key) {
+					case "ArrowLeft":
+					case "ArrowDown":
+						e.preventDefault();
+						setValue(value - (step ?? 1));
+						break;
+					case "ArrowRight":
+					case "ArrowUp":
+						e.preventDefault();
+						setValue(value + (step ?? 1));
+						break;
+					case "Home":
+						e.preventDefault();
+						setValue(min);
+						break;
+					case "End":
+						e.preventDefault();
+						setValue(max);
+						break;
+					case "PageUp":
+						e.preventDefault();
+						if (step != null) setValue(Math.min(value + step * 10, max));
+						break;
+					case "PageDown":
+						e.preventDefault();
+						if (step != null) setValue(Math.max(value - step * 10, min));
+						break;
+				}
 			}}>
 				<div className={styles.fill}/>
 			</div>
