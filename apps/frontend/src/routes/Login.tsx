586da90ad25ecb438393cadf643fab1acca6371d
Matt-hhs <24071765@student.hhs.nl>
2025-12-01 17:43:13 +0100

    Add working Input fields with submit button

--- apps/frontend/src/routes/Login.tsx
+++ apps/frontend/src/routes/Login.tsx
@@ -1,3 +1,41 @@
+import React from 'react';
+import styles from "./Login.module.scss";
+import { useRef } from 'react';
+import Input from "../components/Input";
+import Button from "../components/Button";
+
 export default function Login() {
-  return <div>Login Page</div>;
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const login = async (email: string, password: string) => {
+        const res = await fetch('/identity/login', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({email, password})
+    });
+
+        if (!res.ok) throw new Error("Login failed");
+        const data = await res.json();
+        localStorage.setItem("token", data.token);
+    }
+
+    function handleSubmit () {
+        const email = emailRef.current?.value || "";
+        const password = passwordRef.current?.value || "";
+
+        login(email, password).catch(err => {
+            console.error(err);
+            console.log(email, password);
+            alert("Login failed");
+        });
+    }
+
+    return (
+        <>
+            <Input type="email" placeholder="email" ref={emailRef}/>
+            <Input type="password" placeholder="password" ref={passwordRef}/>
+            <Button onClick={handleSubmit} variant="text" color="brand" disabled={false}>Submit</Button>
+        </>
+    );
 }
