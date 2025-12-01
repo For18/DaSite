b6b8e8df2fb383125b31117a915e2e2c3e807108
TCAGM-Dev <thomas@andringa.org>
2025-12-01 13:55:22 +0100

    Add accordion

--- apps/frontend/src/components/Accordion.tsx
+++ apps/frontend/src/components/Accordion.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren, useEffect, useId, useLayoutEffect, useRef, useState } from "react";
-import { useScreenSize } from "../lib/util";
+import { PropsWithChildren, useLayoutEffect, useRef, useState } from "react";
 import styles from "./Accordion.module.scss";
 import Typography from "./Typography";
+import { useScreenSize } from "../lib/util";
 
 export interface AccordionProps extends PropsWithChildren {
 	title: string;
@@ -13,49 +13,21 @@ export default function Accordion({ title, open = false, onToggle, children }: A
 	const innerContentRef = useRef<HTMLDivElement>(null);
 	const [contentHeight, setContentHeight] = useState<number>(0);
 	const [screenX, screenY] = useScreenSize();
-	const barRef = useRef<HTMLDivElement>(null);
-	const contentId = useId();
-	const headerButtonId = useId();
 
-	function toggle() {
-		onToggle?.(!open);
-	}
-
-	function updateSize() {
+	useLayoutEffect(() => {
 		if (innerContentRef.current == null) return;
 		setContentHeight(innerContentRef.current.getBoundingClientRect().height);
-	}
-	useLayoutEffect(() => {
-		updateSize();
 	}, [open, screenX, screenY, innerContentRef.current, children]);
-	useLayoutEffect(() => {
-		if (innerContentRef.current == null) return;
-		const observer = new ResizeObserver(updateSize);
-		observer.observe(innerContentRef.current);
-		return () => observer.disconnect();
-	}, [innerContentRef.current]);
 
 	return (
-		<div className={styles.container + (open ? " " + styles.open : "")} aria-label={title}>
-			<div role="heading" aria-level={3}>
-				<div id={headerButtonId} className={styles.bar} ref={barRef} onClick={toggle} role="button"
-					aria-controls={contentId} aria-expanded={open} tabIndex={0} onKeyDown={e => {
-					if (e.key !== "Enter" && e.key !== " ") return;
-					e.preventDefault();
-					toggle();
-				}}>
-					<Typography heading={3}>{title}</Typography>
-					<svg viewBox="-7 -10 114 110" className={styles.arrow} role="none">
-						<path d="M100 25 L50 75 L0 25"/>
-					</svg>
-				</div>
+		<div className={styles.container + (open ? " " + styles.open : "")}>
+			<div className={styles.bar} onClick={() => onToggle?.(!open)}>
+				<Typography heading={3}>{title}</Typography>
+				<svg viewBox="-10 -10 110 110" className={styles.arrow}>
+					<path d="M100 25 L50 75 L0 25"/>
+				</svg>
 			</div>
-			<div className={styles.content} style={{ "--content-height": contentHeight } as any} role="region"
-				aria-labelledby={headerButtonId} inert={!open} id={contentId} onKeyDown={e => {
-				if (e.key !== "PageUp") return;
-				e.preventDefault();
-				barRef.current?.focus();
-			}}>
+			<div className={styles.content} style={{"--content-height": contentHeight} as any}>
 				<div className={styles.inner} ref={innerContentRef}>
 					{children}
 				</div>
--- apps/frontend/src/routes/Test.tsx
+++ apps/frontend/src/routes/Test.tsx
@@ -1,11 +1,12 @@
 import { useCallback, useEffect, useState } from "react";
+import Accordion from "../components/Accordion";
 import Button from "../components/Button";
 import Checkbox from "../components/Checkbox";
 import Input from "../components/Input";
 import LabeledContainer from "../components/LabeledContainer";
+import Slider from "../components/Slider";
 import { Switch } from "../components/Switch";
 import Typography from "../components/Typography";
-import Slider from "../components/Slider";
 
 export default function Test() {
 	useEffect(() => {
@@ -19,43 +20,54 @@ export default function Test() {
 	}, []);
 	const [checked, setChecked] = useState<boolean>(false);
 	const [sliderValue, setSliderValue] = useState<number>(50);
+	const [inputsOpen, setInputsOpen] = useState<boolean>(false);
+	const [buttonsOpen, setButtonsOpen] = useState<boolean>(false);
+	const [sliderOpen, setSliderOpen] = useState<boolean>(false);
 
 	return (
 		<>
-			<Typography heading={3}>Buttons pressed: {buttonsPressed}</Typography>
-			<table>
-				<tbody>
-					{["contained", "outlined", "text"].map(variant => (
-						<tr key={variant}>
-							{["brand", "success", "warning", "error"].map(color => (
-								<td key={color}>
-									<Button
-										variant={variant as any}
-										color={color as any}
-										disabled={!buttonsEnabled}
-										onClick={incrementButtonsPressed}
-									>
-										Button
-									</Button>
-								</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<LabeledContainer text="Enable buttons" color={buttonsEnabled ? "primary" : "secondary"} id="switch">
-				<Switch enabled={buttonsEnabled} onClick={() => setButtonsEnabled(v => !v)} labelledby="switch"/>
-			</LabeledContainer>
-			<LabeledContainer text="Disable fields" id="checkbox">
-				<Checkbox checked={checked} onClick={() => setChecked(v => !v)} labelledby="checkbox"/>
-			</LabeledContainer>
-			{["text", "password", "date", "datetime-local", "email", "number", "tel", "time", "url"].map(type => (
-				<LabeledContainer key={type} text={type} color={checked ? "secondary" : "primary"} id={`field-${type}`}>
-					<Input type={type as any} placeholder="placeholder" disabled={checked}
-						labelledby={`field-${type}`}/>
+			<Accordion title="Buttons" open={buttonsOpen} onToggle={setButtonsOpen}>
+				<Typography heading={3}>Buttons pressed: {buttonsPressed}</Typography>
+				<table>
+					<tbody>
+						{["contained", "outlined", "text"].map(variant => (
+							<tr key={variant}>
+								{["brand", "success", "warning", "error"].map(color => (
+									<td key={color}>
+										<Button
+											variant={variant as any}
+											color={color as any}
+											disabled={!buttonsEnabled}
+											onClick={incrementButtonsPressed}
+										>
+											Button
+										</Button>
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+				<LabeledContainer text="Enable buttons" color={buttonsEnabled ? "primary" : "secondary"} id="switch">
+					<Switch enabled={buttonsEnabled} onClick={() => setButtonsEnabled(v => !v)} labelledby="switch"/>
 				</LabeledContainer>
-			))}
-			<Slider min={0} max={100} value={sliderValue} onChange={setSliderValue} width="500px" step={10}/>
+			</Accordion>
+			<Accordion title="Inputs" open={inputsOpen} onToggle={setInputsOpen}>
+				<LabeledContainer text="Disable fields" id="checkbox">
+					<Checkbox checked={checked} onClick={() => setChecked(v => !v)} labelledby="checkbox"/>
+				</LabeledContainer>
+				{["text", "password", "date", "datetime-local", "email", "number", "tel", "time", "url"].map(type => (
+					<LabeledContainer key={type} text={type} color={checked ? "secondary" : "primary"}
+						id={`field-${type}`}
+					>
+						<Input type={type as any} placeholder="placeholder" disabled={checked}
+							labelledby={`field-${type}`}/>
+					</LabeledContainer>
+				))}
+			</Accordion>
+			<Accordion title="Slider" open={sliderOpen} onToggle={setSliderOpen}>
+				<Slider min={0} max={100} value={sliderValue} onChange={setSliderValue} width="500px" step={10}/>
+			</Accordion>
 		</>
 	);
 }
