3e7352e0137464eafa6ece2e38a9103eff262860
TCAGM-Dev <thomas@andringa.org>
2025-12-01 13:55:22 +0100

    Add accordion

---
+++ apps/frontend/src/components/Accordion.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren, useLayoutEffect, useRef, useState } from "react";
+import styles from "./Accordion.module.scss";
+import Typography from "./Typography";
+import { useScreenSize } from "../lib/util";
+
+export interface AccordionProps extends PropsWithChildren {
+	title: string;
+	open: boolean;
+	onToggle?: (isOpen: boolean) => void;
+}
+
+export default function Accordion({ title, open = false, onToggle, children }: AccordionProps) {
+	const innerContentRef = useRef<HTMLDivElement>(null);
+	const [contentHeight, setContentHeight] = useState<number>(0);
+	const [screenX, screenY] = useScreenSize();
+
+	useLayoutEffect(() => {
+		if (innerContentRef.current == null) return;
+		setContentHeight(innerContentRef.current.getBoundingClientRect().height);
+	}, [open, screenX, screenY, innerContentRef.current, children]);
+
+	return (
+		<div className={styles.container + (open ? " " + styles.open : "")}>
+			<div className={styles.bar} onClick={() => onToggle?.(!open)}>
+				<Typography heading={3}>{title}</Typography>
+				<svg viewBox="-10 -10 110 110" className={styles.arrow}>
+					<path d="M100 25 L50 75 L0 25"/>
+				</svg>
+			</div>
+			<div className={styles.content} style={{"--content-height": contentHeight} as any}>
+				<div className={styles.inner} ref={innerContentRef}>
+					{children}
+				</div>
+			</div>
+		</div>
+	);
+}
