e1f5626dc2782e43e7ba0a87aa04861e255b3ba4
TCAGM-Dev <thomas@andringa.org>
2025-12-01 18:41:00 +0100

    Improve Accordion accessablility

--- apps/frontend/src/components/Accordion.tsx
+++ apps/frontend/src/components/Accordion.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useLayoutEffect, useRef, useState } from "react";
+import { PropsWithChildren, useId, useLayoutEffect, useRef, useState } from "react";
 import styles from "./Accordion.module.scss";
 import Typography from "./Typography";
 import { useScreenSize } from "../lib/util";
@@ -13,6 +13,7 @@ export default function Accordion({ title, open = false, onToggle, children }: A
 	const innerContentRef = useRef<HTMLDivElement>(null);
 	const [contentHeight, setContentHeight] = useState<number>(0);
 	const [screenX, screenY] = useScreenSize();
+	const contentId = useId();
 
 	useLayoutEffect(() => {
 		if (innerContentRef.current == null) return;
@@ -21,13 +22,13 @@ export default function Accordion({ title, open = false, onToggle, children }: A
 
 	return (
 		<div className={styles.container + (open ? " " + styles.open : "")}>
-			<div className={styles.bar} onClick={() => onToggle?.(!open)}>
+			<div className={styles.bar} onClick={() => onToggle?.(!open)} aria-owns={contentId}>
 				<Typography heading={3}>{title}</Typography>
 				<svg viewBox="-10 -10 110 110" className={styles.arrow}>
 					<path d="M100 25 L50 75 L0 25"/>
 				</svg>
 			</div>
-			<div className={styles.content} style={{"--content-height": contentHeight} as any} inert={!open}>
+			<div className={styles.content} style={{"--content-height": contentHeight} as any} inert={!open} aria-expanded={open} id={contentId}>
 				<div className={styles.inner} ref={innerContentRef}>
 					{children}
 				</div>
