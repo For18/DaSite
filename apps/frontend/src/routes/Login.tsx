019ddfcaf0fb038410d3313f30bada662ddf1a54
Matt-hhs <24071765@student.hhs.nl>
2025-12-02 11:13:12 +0100

    Add function refreshToken + include credentials in login

--- apps/frontend/src/routes/Login.tsx
+++ apps/frontend/src/routes/Login.tsx
@@ -8,16 +8,26 @@ export default function Login() {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
 
+    async function refreshToken() {
+        const res = await fetch('/identity/refresh', {
+            method: "POST",
+            credentials: "include"
+        });
+
+        if (!res.ok) throw new Error("Token refresh failed");
+        const data = await res.json();
+        (window as any).accessToken = data.accessToken;
+    }
+
     const login = async (email: string, password: string) => {
         const res = await fetch('/identity/login', {
             method: "POST",
+            credentials: "include",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({email, password})
     });
 
         if (!res.ok) throw new Error("Login failed");
-        const data = await res.json();
-        localStorage.setItem("token", data.token);
     }
 
     function handleSubmit () {
