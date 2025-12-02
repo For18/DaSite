bd467e90a293c8804e2d3f02fcfae89b7eab081d
Matt-hhs <24071765@student.hhs.nl>
2025-12-02 10:36:00 +0100

    Add OnModelCreating + Correct imports

--- apps/backend/DatabaseContext.cs
+++ apps/backend/DatabaseContext.cs
@@ -1,12 +1,19 @@
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 public class DatabaseContext : IdentityDbContext<User> {
 	public DbSet<Product> Products { get; set; }
 	public DbSet<ProductImage> ProductImages { get; set; }
 	public DbSet<Auction> Auctions { get; set; }
 	public DbSet<Sale> Sales { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
 
 	protected override void OnConfiguring(DbContextOptionsBuilder contextBuilder) {
 		contextBuilder.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING") ?? "");
