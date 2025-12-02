62e8fee3a8bf6267b92e05118811f56671344c49
TCAGM-Dev <thomas@andringa.org>
2025-12-02 16:56:02 +0100

    Make Checkbox animation perfect

--- apps/frontend/src/components/Checkbox.tsx
+++ apps/frontend/src/components/Checkbox.tsx
@@ -17,7 +17,7 @@ export default function Checkbox({ checked, onClick, labelledby }: CheckboxProps
 			aria-labelledby={labelledby} className={styles.container + (checked ? " " + styles.checked : "")}
 		>
 			<svg className={styles.svg} viewBox="-20 -20 130 130" role="none">
-				<path d="M100 0 L50 90 L0 50"/>
+				<path d="M100 0 L50 90 L0 50" pathLength="1"/>
 			</svg>
 		</div>
 	);
