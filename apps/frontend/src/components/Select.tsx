eaf883906dbd2b0ccc43ff4faa8bae1af9c606ba
TCAGM-Dev <thomas@andringa.org>
2025-12-06 10:14:02 +0100

    Add Select

--- apps/frontend/src/components/Select.tsx
+++ apps/frontend/src/components/Select.tsx
@@ -1,12 +1,11 @@
-import { createContext, Key, PropsWithChildren, useCallback, useContext, useEffect, useId, useLayoutEffect, useRef,
-	useState } from "react";
+import { createContext, Key, PropsWithChildren, useCallback, useContext, useEffect, useLayoutEffect, useRef,
+	useState, useId } from "react";
 import ClickAwayDetector from "./ClickAwayDetector";
 import styles from "./Select.module.scss";
 import Typography from "./Typography";
 
 // TODO: Add checkmark next to selected option
 // TODO: Add opening arrow to head
-// TODO: Fix opening via click
 
 export interface SelectContext {
 	value: string | null;
@@ -27,9 +26,7 @@ export interface SelectProps extends PropsWithChildren {
 	"aria-labelledby"?: string;
 }
 
-export function Select(
-	{ children, value, onChange: setValue, placeholder, "aria-labelledby": labelledBy }: SelectProps
-) {
+export function Select({ children, value, onChange: setValue, placeholder, "aria-labelledby": labelledBy }: SelectProps) {
 	const [open, setOpen] = useState<boolean>(false);
 	const innerRef = useRef<HTMLDivElement>(null);
 	const [contentHeight, setContentHeight] = useState<number>(0);
@@ -43,34 +40,34 @@ export function Select(
 	}, [innerRef, children]);
 
 	const focusFirst = useCallback(() => {
-		(innerRef.current?.firstElementChild as HTMLElement | null)?.focus();
+		(innerRef.current?.firstElementChild as HTMLElement | null)?.focus()
 	}, [innerRef]);
 	const focusLast = useCallback(() => {
-		(innerRef.current?.lastElementChild as HTMLElement | null)?.focus();
+		(innerRef.current?.lastElementChild as HTMLElement | null)?.focus()
 	}, [innerRef]);
 
 	return (
 		<ClickAwayDetector onClickAway={() => setOpen(false)}>
 			<div className={styles.container}>
-				<div className={styles.head} ref={headRef} onClick={() => setOpen(!open)} tabIndex={0}
-					onFocus={_e => setOpen(true)} onKeyDown={e => {
+				<div className={styles.head} ref={headRef} onClick={() => setOpen(!open)} tabIndex={0} onFocus={_e => setOpen(true)} onKeyDown={e => {
 					if (e.key === "Tab") setOpen(false);
 					else if (e.key === "ArrowDown" || e.key === "ArrowUp") {
 						e.preventDefault();
 						setOpen(true);
 						focusFirst();
-					} else if (e.key === "Escape") setOpen(false);
+					}
+					else if (e.key === "Escape") setOpen(false);
 					else if (e.key === "Enter" || e.key === " ") setOpen(!open);
 					else if (e.key === "Home") {
 						setOpen(true);
 						focusFirst();
-					} else if (e.key === "End") {
+					}
+					else if (e.key === "End") {
 						setOpen(true);
 						focusLast();
 					}
 					// TODO: Add filtering by typing
-				}} role="combobox" aria-labelledby={labelledBy} aria-controls={innerId} aria-expanded={open}>
-					{/* TODO: Add aria-activedescendant */}
+				}} role="combobox" aria-labelledby={labelledBy} aria-controls={innerId} aria-expanded={open}> {/* TODO: Add aria-activedescendant */}
 					<Typography>{value ?? placeholder}</Typography>
 				</div>
 				<div className={styles.dropdown + (open ? " " + styles.open : "")} style={{
@@ -119,17 +116,18 @@ export function Option({ value, children, key }: OptionProps) {
 	}, [setSelectValue, value]);
 
 	return (
-		<div key={key} ref={containerRef} className={styles.item + (selectValue === value ? " " + styles.selected : "")}
-			onClick={select} tabIndex={-1} onKeyDown={e => {
+		<div key={key} ref={containerRef} className={styles.item + (selectValue === value ? " " + styles.selected : "")} onClick={select} tabIndex={-1} onKeyDown={e => {
 			if (e.key === "Enter" || e.key === " ") {
 				e.preventDefault();
 				select();
 				focusHead();
 				setOpen(false);
-			} else if (e.key === "ArrowDown") {
+			}
+			else if (e.key === "ArrowDown") {
 				e.preventDefault();
 				(containerRef.current?.nextElementSibling as HTMLElement | null)?.focus();
-			} else if (e.key === "ArrowUp") {
+			}
+			else if (e.key === "ArrowUp") {
 				e.preventDefault();
 				if (e.altKey) {
 					select();
@@ -138,16 +136,19 @@ export function Option({ value, children, key }: OptionProps) {
 				} else {
 					(containerRef.current?.previousElementSibling as HTMLElement | null)?.focus();
 				}
-			} else if (e.key === "Tab") {
+			}
+			else if (e.key === "Tab") {
 				select();
 				setOpen(false);
-			} else if (e.key === "Escape") {
+			}
+			else if (e.key === "Escape") {
 				focusHead();
 				setOpen(false);
-			} else if (e.key === "Home" || e.key === "PageUp") {
-				e.stopPropagation();
+			}
+			else if (e.key === "Home" || e.key === "PageUp") {
 				focusFirstOption();
-			} else if (e.key === "End" || e.key === "PageDown") {
+			}
+			else if (e.key === "End" || e.key === "PageDown") {
 				focusLastOption();
 			}
 			// TODO: Add filtering by typing
