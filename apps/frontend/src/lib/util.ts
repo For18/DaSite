ea2aa9cec4a62a4c528810cbd5e24bb0ce3a4aa9
Julius Schutte <Julius.Schutte@gmail.com>
2025-12-10 14:02:34 +0100

    Remove duplicate function

--- apps/frontend/src/lib/util.ts
+++ apps/frontend/src/lib/util.ts
@@ -175,33 +175,3 @@ export function useMousePosition(): [number, number] {
 
 	return mousePosition;
 }
-
-export function useMousePosition(): [number, number] {
-	const [mousePosition, setMousePosition] = useState<[number, number]>([0, 0]);
-
-	function listener(e: MouseEvent) {
-		setMousePosition([e.clientX, e.clientY]);
-	}
-
-	useEffect(() => {
-		window.addEventListener("mousemove", listener);
-		return () => window.removeEventListener("mousemove", listener);
-	}, []);
-
-	return mousePosition;
-}
-
-export function useMousePosition(): [number, number] {
-	const [mousePosition, setMousePosition] = useState<[number, number]>([0, 0]);
-
-	function listener(e: MouseEvent) {
-		setMousePosition([e.clientX, e.clientY]);
-	}
-
-	useEffect(() => {
-		window.addEventListener("mousemove", listener);
-		return () => window.removeEventListener("mousemove", listener);
-	}, []);
-
-	return mousePosition;
-}
