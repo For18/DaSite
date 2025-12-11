ce43fb3272b7853768eab5f6778e29b6338c2081
TCAGM-Dev <thomas@andringa.org>
2025-12-11 12:54:01 +0100

    Add Checkbox.cy.tsx

---
+++ apps/frontend/src/components/Checkbox.cy.tsx
@@ -0,0 +1,15 @@
+import Checkbox from "./Checkbox";
+
+describe("<Checkbox/>", () => {
+	it("renders", () => {
+		cy.mount(<Checkbox checked={false} onClick={() => {}}/>);
+	})
+
+	it("can be toggled", () => {
+		let value = false;
+		cy.mount(<Checkbox checked={value} onClick={() => value = !value}/>);
+		cy.get("[role=checkbox]").click().then(() => {
+			expect(value).to.be.true;
+		});
+	})
+});
