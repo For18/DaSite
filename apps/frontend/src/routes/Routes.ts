840f1b4105caf4ae120c4cc5ea3b0de105886bfc
Julius Schutte <Julius.Schutte@gmail.com>
2025-12-12 14:11:43 +0100

    Add BatchPost API endpoint to AuctionController

--- apps/frontend/src/routes/Routes.ts
+++ apps/frontend/src/routes/Routes.ts
@@ -85,6 +85,7 @@ export const Routes = {
     GetAll:          `/auctions`,
     GetUpcoming:     `/auctions/upcoming`,
     Post:            `/auction`,
+    BatchPost:       `/auctions/batch`,
     Delete:          (id: number | string) => `/auction/${id}`,
     Patch:           (id: number | string) => `/auction/${id}`,
   },
