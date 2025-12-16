4852af21666026fed3c10e5aa3da69587463093d
Julius Schutte <Julius.Schutte@gmail.com>
2025-12-16 10:48:19 +0100

    Add card to see current user

--- apps/frontend/src/components/TopBar.tsx
+++ apps/frontend/src/components/TopBar.tsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router";
+import { PublicUser, API_URL } from "../lib/api";
+import { usePromise } from "../lib/util";
 import Button from "./Button";
 import styles from "./TopBar.module.scss";
+import Image from "./Image";
+import { Routes } from "../routes/Routes";
 
 export default function TopBar({
 	links
@@ -8,6 +12,24 @@ export default function TopBar({
 	links: { [name: string]: string };
 }) {
 	const navigate = useNavigate();
+  const currentUserPromise = usePromise<PublicUser>(async () => {
+      return fetch(API_URL + Routes.User.GetCurrent)
+        .then(response => {
+            if (response.status == 401) throw new Error("No logged user");
+            else if (response.status != 200) throw new Error("Error: " + response.status)
+
+            return response.json();
+        })
+        .then(data => data as PublicUser)
+        .then(user => user);
+  }, []);
+
+  // TODO: find better placeholder (build in <Image> placeholder doesn't work)
+  let pfpUrl = "https://www.shutterstock.com/image-vector/highresolution-default-profile-avatar-icon-260nw-2600268263.jpg";
+  if (currentUserPromise.error) { console.error(currentUserPromise.error) }
+  else if(currentUserPromise.value != null && currentUserPromise.value.avatarImageUrl != null) {
+    pfpUrl = currentUserPromise.value.avatarImageUrl
+  }
 
 	return (
 		<header className={styles.header}>
@@ -15,7 +37,7 @@ export default function TopBar({
 				{Object.entries(links).map(([name, path]) => (
 					<Button
 						key={name}
-						onClick={e => {
+						onClick={_ => {
 							navigate(path);
 						}}
 					>
@@ -23,6 +45,20 @@ export default function TopBar({
 					</Button>
 				))}
 			</nav>
+      <Image
+        className={styles.profile}
+        src={pfpUrl}
+        alt={"User Profile"}
+        width={50}
+        height={50}
+        onClick={() => {
+          if (currentUserPromise.value){
+            navigate(Routes.Pages.Profile(currentUserPromise.value.id));
+          } else {
+            navigate(Routes.Pages.Login);
+          }
+        }}
+      />
 		</header>
 	);
 }
