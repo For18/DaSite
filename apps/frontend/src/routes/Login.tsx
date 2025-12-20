505f037d2b50fbbed67cc9367c42a38311636664
Julius Schutte <Julius.Schutte@gmail.com>
2025-12-20 15:58:17 +0100

    Update login page to make use of useAuth's login() function

--- apps/frontend/src/routes/Login.tsx
+++ apps/frontend/src/routes/Login.tsx
@@ -3,46 +3,27 @@ import { useNavigate } from "react-router";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import Typography from "../components/Typography";
-import { API_URL } from "../lib/api";
 import styles from "./AuthForm.module.scss";
 import { Routes } from "./Routes";
+import useAuth from "../AuthProvider";
 
 export default function Login() {
 	const [email, setEmail] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
 	const navigate = useNavigate();
 	const passwordRef = useRef<HTMLInputElement>(null);
+  const { login, user } = useAuth();
 
-	async function login(email: string, password: string) {
-		const res = await fetch(API_URL + Routes.Identity.PostLogin + "?useCookies=true", {
-			method: "POST",
-			credentials: "include",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ email, password })
-		});
-
-		const text = await res.text();
-		let data;
-
-		try {
-			data = JSON.parse(text);
-		} catch {
-			data = text;
-		}
-
-		return { works: res.ok, httpStatus: res.status, data };
-	}
 	async function handleSubmit() {
-		const { works, httpStatus, data } = await login(email, password);
-
-		if (!works) {
-			console.error("Registration failed:", httpStatus, data);
-			return;
-		}
-
-		console.log("Login successful:", httpStatus, data);
-		navigate(Routes.Pages.Home);
+    await login(email, password)
 	}
+
+  useEffect(() => {
+      if (user) {
+        console.log("Login successful!: ", user);
+        navigate(Routes.Pages.Home);
+      }
+  }, [user])
 
 	useEffect(() => {
 		document.title = "For18 - Login";
