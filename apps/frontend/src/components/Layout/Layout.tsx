c606b94e59899c33650b92e1db6166be72d17721
Julius Schutte <Julius.Schutte@gmail.com>
2025-12-24 02:59:02 +0100

    Update Layout to show different hyperlinks per user type

--- apps/frontend/src/components/Layout/Layout.tsx
+++ apps/frontend/src/components/Layout/Layout.tsx
@@ -4,25 +4,42 @@ import { Routes } from "@route/Routes";
 import { type PropsWithChildren } from "react";
 import { useLocation } from "react-router";
 import styles from "./Layout.module.scss";
+import { UserType, useAPI } from "@/lib/api";
+import useAuth from "@/AuthProvider";
 
 export default function Layout({ children }: PropsWithChildren) {
 	const location = useLocation();
 	const homepage = ["/"];
+  const userType = useAPI<UserType>(Routes.User.GetUserType);
+  const { user } = useAuth();
+
+  let hyperlinksToRender: Record<string, string>[] = [
+    {title: "Home", link: Routes.Pages.Home}
+  ];
+
+  if (!userType) hyperlinksToRender.push({title: "Login", link: Routes.Pages.Login});
+  else {
+    hyperlinksToRender.push(
+        {title: "Auctions", link: Routes.Pages.Auctions.Base},
+        {title: "Profile", link: user ? Routes.Pages.Profile(user.id) : null}
+    )
+    if (userType != "Customer") { 
+      hyperlinksToRender.push(
+        {title: "PendingAuctions", link: Routes.Pages.Auctions.Pending},
+        {title: "CreateAuction", link: Routes.Pages.Auctions.Create},
+        {title: "CreateProduct", link: Routes.Pages.CreateProduct},
+      )
+    }
+  }
 
 	const isHomepage = homepage.includes(location.pathname);
 
 	return (
 		<div className={styles.container}>
 			<TopBar
-				links={{
-					Home: Routes.Pages.Home,
-					Auctions: Routes.Pages.Auctions.Base,
-					PendingAuction: Routes.Pages.Auctions.Pending,
-					Profile: Routes.Pages.Profile(1),
-					CreateAuction: Routes.Pages.Auctions.Create,
-					Login: Routes.Pages.Login
-				}}
-			/>
+				links={
+        Object.fromEntries(hyperlinksToRender.filter(link => link.link != null).map(link => [link.title, link.link]))
+      }/>
 			<main className={isHomepage ? styles.homepageArticle : styles.article}>
 				{children}
 			</main>
