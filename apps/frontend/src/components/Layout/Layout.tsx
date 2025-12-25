1597a43938fcbeca92e5a3b7075c8a9dff467fea
Julius Schutte <Julius.Schutte@gmail.com>
2025-12-25 11:45:25 +0100

    Update Layout to use the user type provided by useAuth

--- apps/frontend/src/components/Layout/Layout.tsx
+++ apps/frontend/src/components/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import useAuth from "@/AuthProvider";
-import { useAPI, UserType } from "@/lib/api";
 import Footer from "@component/Footer";
 import TopBar from "@component/TopBar";
 import { Routes } from "@route/Routes";
@@ -10,20 +9,19 @@ import styles from "./Layout.module.scss";
 export default function Layout({ children }: PropsWithChildren) {
 	const location = useLocation();
 	const homepage = ["/"];
-	const userType = useAPI<UserType>(Routes.User.GetUserType);
-	const { user } = useAuth();
+	const { user, role } = useAuth();
 
 	let hyperlinksToRender: Record<string, string>[] = [
 		{ title: "Home", link: Routes.Pages.Home }
 	];
 
-	if (!userType) hyperlinksToRender.push({ title: "Login", link: Routes.Pages.Login });
+	if (!role) hyperlinksToRender.push({ title: "Login", link: Routes.Pages.Login });
 	else {
 		hyperlinksToRender.push(
 			{ title: "Auctions", link: Routes.Pages.Auctions.Base },
 			{ title: "Profile", link: user ? Routes.Pages.Profile(user.id) : null }
 		);
-		if (userType != "Customer") {
+		if (role == "Admin" || role == "AuctionMaster") {
 			hyperlinksToRender.push(
 				{ title: "PendingAuctions", link: Routes.Pages.Auctions.Pending },
 				{ title: "CreateAuction", link: Routes.Pages.Auctions.Create },
