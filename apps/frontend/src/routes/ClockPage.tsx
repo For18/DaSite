b07049521d87ff042e3aea0d9e5c6672a47ebfe4
TCAGM-Dev <thomas@andringa.org>
2026-01-05 09:59:50 +0100

    Fix clock page layout

--- apps/frontend/src/routes/ClockPage.tsx
+++ apps/frontend/src/routes/ClockPage.tsx
@@ -170,7 +170,6 @@ export default function ClockPage() {
 				/>
 
 				<Button
-					className={styles.button}
 					variant="outlined"
 					disabled={progress < 0 || progress > 1 || isBuffered}
 					onClick={() => {
