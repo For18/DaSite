2bfa3a84e2544ecc650be9e1daf9ad21f435bda7
TCAGM-Dev <thomas@andringa.org>
2026-01-05 10:18:56 +0100

    Add deve elements functionality

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,7 +3,16 @@ import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react({})],
+	plugins: [react({}), {
+		name: "dev-elements",
+		transformIndexHtml(html: string, ctx) {
+			if (ctx?.server == null) { // Production
+				return html.replace(/<\s*vite-dev\s*>([\s\S]*?)<\s*\/\s*vite-dev\s*>/g, "");
+			} else { // Development
+				return html.replace(/<\s*vite-dev\s*>([\s\S]*?)<\s*\/\s*vite-dev\s*>/g, "$1");
+			}
+		}
+	}],
 	css: {
 		modules: {
 			localsConvention: "camelCase"
