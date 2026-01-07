0b3ee7127ff241cec921022053724627b94884ce
TCAGM-Dev <thomas@andringa.org>
2026-01-07 11:51:29 +0100

    Start with e2e dev server

--- apps/frontend/cypress.config.ts
+++ apps/frontend/cypress.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
 	e2e: {
 		baseUrl: "http://localhost:5173",
 		setupNodeEvents(on, config) {
+			// on("dev-server:start", (options) => { // TODO: Fix
+			// 	// docker build -t for18-frontend . & docker run -e VITE_API_URL="/api/v1" -p 5173:5173 for18-frontend
+			// })
 			on("file:preprocessor", vitePreprocessor());
 		}
 	},
