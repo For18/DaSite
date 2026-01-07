f58f380309d447eea0d44e1104cc603681b3e53a
TCAGM-Dev <thomas@andringa.org>
2026-01-07 11:53:27 +0100

    Add Switch test

---
+++ apps/frontend/src/components/Switch/Switch.cy.tsx
@@ -0,0 +1,36 @@
+import Switch from "./Switch";
+
+describe("<Switch/>", () => {
+    it("renders", () =>  {
+        cy.mount(<Switch enabled={true} onClick={() => {}}/>);
+    });
+
+    it("sets enabled as aria-checked", () => {
+        cy.mount(<Switch enabled={false} onClick={() => {}}/>);
+
+        cy.get('[role="switch"]').should("have.attr", "aria-checked", "false");
+    });
+
+    it("calls onClick() on enter and space", () => {
+        const onClick = cy.stub();
+        let clicked = false;
+
+        cy.mount(<Switch enabled={true} onClick={() => clicked = true}/>);
+
+        cy.get('[role="switch"]').type('{enter}').then(() => {
+			expect(clicked).to.be.true;
+		});
+		clicked = false;
+
+        cy.get('[role="switch"]').type(' ').then(() => {
+			expect(clicked).to.be.true;
+		});
+		clicked = false;
+    });
+
+    it("doesn't apply enabled class when not enabled", () => {
+        cy.mount(<Switch enabled={false} onClick={() => {}}/>);
+
+        cy.get('[role="switch"]').should("not.have.class", "styles.enabled");
+    });
+});
