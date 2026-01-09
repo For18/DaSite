d1651a75738b7ff46f1c8d55c38daeb02613c91b
Matt-hhs <24071765@student.hhs.nl>
2026-01-09 18:08:08 +0100

    Feature proper time display for auction

--- apps/frontend/src/routes/Auctions.tsx
+++ apps/frontend/src/routes/Auctions.tsx
@@ -62,7 +62,7 @@ export default function Auctions() {
 		const minutes = Math.floor((totalSeconds % 3600) / 60);
 		const seconds = totalSeconds % 60;
 
-		return `${hours}:${minutes}:${seconds}`;
+		return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 	}
 
 	return (
